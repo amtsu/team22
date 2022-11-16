8a65d093a28a776e0d0540f7446f85fad8ccc053
amtsurkov <amtsurkov@malvina81.fvds.ru>
2022-11-16 21:09:05 +0300

    add lesson 19

---
+++ users/amtsurkov/lesson_19/lesson19.py
@@ -0,0 +1,11 @@
+print('hello')
+x = 10
+print(x + 2)
+
+def main():
+    print('main start')
+    
+print("__name__ in 19=", __name__)
+    
+if __name__== "__main__":
+    main()
---
+++ users/amtsurkov/lesson_19/lesson19_run.py
@@ -0,0 +1,14 @@
+import lesson19
+
+import lesson19_q2
+
+print("__name__ in run =", __name__)
+
+print('end')
+
+f = 34
+
+if f > 4 :
+    lesson19_q2.q2()
+    
+print(lesson19.x)
