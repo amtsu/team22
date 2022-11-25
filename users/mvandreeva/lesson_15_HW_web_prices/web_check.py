eff46a367e8852fa0a83416df5a97eabc79f503a
amtsu <113842280+amtsu@users.noreply.github.com>
2022-11-25 11:45:43 +0300

    HW_15 (#52)
    Co-authored-by: mvandreeva <Mariya.V.Andreeva@yandex.ru>

---
+++ users/mvandreeva/lesson_15_HW_web_prices/web_check.py
@@ -0,0 +1,98 @@
+import urllib
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+
+class CheckPrice():
+
+    def get_price(self, tag, class_name):
+        page = urllib.urlopen(self.__url)
+        if page.getcode() == 200:
+            text = page.read()
+            if not text == []:
+                soup = BeautifulSoup(text)
+                price_list = soup.findAll(tag, class_= class_name)
+                prices = []
+                for i in range(len(price_list)):
+                    prices.append(price_list[i].text)
+                    print ((i+1), price_list[i].text)
+                return prices
+            else:
+                print "Text download failed"
+        else:
+            print "Failed to download page"
+    
+    def read_page(self):
+        page = urllib.urlopen(self.__url)
+        print ('Page code: ', page.getcode())
+        text = page.read()
+        return text
+    
+    def __init__(self, link):
+        self.__url = link
+            
+    def __repr__(self):
+        return self.__url
+    
+    def __str__(self):
+        return self.__url
+    
+class ClearData():
+    
+    def clean_data(self, remove, to_int = False):
+        if to_int:
+            data = self.__data.replace(remove, '')
+            data = int(data)
+        else:
+            data = self.__data.replace(remove, '')
+        return data
+    
+    def __init__(self, data):
+        self.__data = data
+            
+    def __repr__(self):
+        return self.__data
+    
+    def __str__(self):
+        return self.__data
+    
+class ClearDataChitaiGorod(ClearData):
+    
+    def clean_data_chitai_gorod(self):
+        data = self.__data.replace(u'\n          ', '')
+        data = self.__data.replace(u'\xa0', '')
+        data = self.__data.replace(u'\u20bd\n', '')
+        data = int(data)
+        return data
+    
+    def __init__(self, data):
+        self.__data = data
+            
+    def __repr__(self):
+        return self.__data
+    
+    def __str__(self):
+        return self.__data
+    
+    
+class SendMail():
+    
+    def __init__(self, price):
+        self.__price = price
+
+    def is_discount(self, discount_price, from_whom, to_whom, subject = 'The price discount test'):
+        if self.__price < discount_price:
+            msg = MIMEText('Price less then %s' % discount_price)
+        else:
+            msg = MIMEText('Price is higher then %s' % discount_price)
+
+        me = from_whom
+        you = to_whom
+
+        msg['Subject'] = subject
+        msg['From'] = me
+        msg['To'] = you
+
+        s = smtplib.SMTP('localhost')
+        s.sendmail(me, [you], msg.as_string())
+        s.quit()
