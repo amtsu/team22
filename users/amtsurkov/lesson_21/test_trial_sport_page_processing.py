e19c6ca567f480cd9112cee88bab78e3072952c5
amtsu <jupyter-amtsu@vm6.template>
2022-11-26 18:27:39 +0300

    add test from read from file

--- users/amtsurkov/lesson_21/test_trial_sport_page_processing.py
+++ users/amtsurkov/lesson_21/test_trial_sport_page_processing.py
@@ -1,4 +1,5 @@
-from TrialSportPageProcessing import OnePage
+import pytest
+from TrialSportPageProcessing import OnePage, OnePageProcessor
 
 #def test_price_goods_1179889():
 #    import sys
@@ -13,8 +14,28 @@ from TrialSportPageProcessing import OnePage
 #    p = OnePage("https://trial-sport.ru/goods/51530/1179889.html")
 #    assert p.get_title() == 'Блин вратаря DR X6 GOALIE BLOKER'
 
-def test_title_goods_1179889():
+def test_web_page_title_goods_1179889():
     p = OnePage("https://trial-sport.ru/goods/51530/1179889.html")
+    expected = [
+        {
+            "title": 'Блин вратаря DR X6 GOALIE BLOKER',
+            "price": 2402,
+        },
+    ]
+    assert p.list_dict() == expected
+
+
+@pytest.fixture()
+def read_from_flie_responce_goods_1179889():
+    #before write code you shell download responce ttps://trial-sport.ru/goods/51530/1179889.html 
+    #curl https://trial-sport.ru/goods/51530/1179889.html -O
+    text = ''
+    with open('download/1179889.html','r') as response:
+        text = response.read() 
+    return text
+
+def test_page_title_goods_1179889(read_from_flie_responce_goods_1179889):
+    p = OnePageProcessor(read_from_flie_responce_goods_1179889)
     assert p.list_dict() == [
         {
             "title": 'Блин вратаря DR X6 GOALIE BLOKER',
