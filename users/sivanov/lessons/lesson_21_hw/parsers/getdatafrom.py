5f3fee9ea6bafbb4e661069e383fddc0827d0d05
Serg (sivanov) <isergy@mail.ru>
2022-12-06 19:25:00 +0300

    added some tests for parser, thinking about refactor some structure

--- users/sivanov/lessons/lesson_21_hw/parsers/getdatafrom.py
+++ users/sivanov/lessons/lesson_21_hw/parsers/getdatafrom.py
@@ -1,11 +1,15 @@
 #!/usr/local/bin/python
 # coding: utf-8
+"""
+модуль, реализующий обработку веб-страниц. Загрузка, анализ, выделение интересующей информации
+"""
 import urllib.request
 from bs4 import BeautifulSoup
 import pytest
-#from usefulstuff import LocalLog
+from usefulstuff import LocalLog
 #from usefulstuff import ColoredStr
 #from usefulstuff import HandmadeTestDecorator
+llog = LocalLog(False)
 class webpage:
     """
     класс вебстраница
@@ -14,86 +18,57 @@ class webpage:
     метод "is_open" сообщает о успехе или неудачи загрузки
     метод text возвращает полученный методом open результат
     """
-    def __init__(self,url=""):
+    def __init__(self,url=''):
         self.__url = url
         self.__is_opened = False
-        self.__last_error = 0
+        self.__last_HTTP_error = 0
+        self.__last_URL_error = 0
+        self.__text = ''
         return None
     #--------------------------------------------
+    @property
+    def last_HTTP_error_code(self):
+        return self.__last_HTTP_error
+    #--------------------------------------------
+    @property
+    def last_URL_error_code(self):
+        return self.__last_URL_error
+    
+    #--------------------------------------------
     def __str__(self):
-        return 'url: ' + self.__url + ', opened: '+ str(self.__is_opened) + ', error code: '+ str(self.__last_error)
+        return ('url: %s, opened: %s, , error codes: HTTP: %d, URL: %d' % 
+                (self.__url, 
+                 str(self.__is_opened),
+                 self.__last_HTTP_error,
+                 self.__last_URL_error) )
     #--------------------------------------------
     def open(self):
-        page = urllib.request.urlopen(self.__url)
-        self.__last_error = page.getcode()
-        if((self.__last_error == 200) or (self.__last_error == None)):
-            # все ок
+        try:
+            page = urllib.request.urlopen(self.__url)
+        except urllib.error.HTTPError as e:
+            self.__last_HTTP_error = e.code
+            self.__text = ''
+            self.__is_opened = False
+            pass
+        except urllib.error.URLError as e:
+            self.__last_URL_error = e.reason
+            self.__text = ''
+            self.__is_opened = False
+            pass
+        else:
+            #self.__last_HTTP_error =  # TODO: разобраться что с этим делать
+            self.__last_URL_error = page.status 
             self.__text = page.read()
             self.__is_opened = True
-        else:
-            self.__text = 'page not opened, error: ' + str(self.__last_error)
-            self.__is_opened = False
         return  self.__text   
     #--------------------------------------------
+    @property
     def text(self):
         return self.__text   
     #--------------------------------------------
     def is_open(self):
         return self.__is_opened
     #--------------------------------------------
-    pass
-    
-
-
-#==================================================================================================================================
-class get_data_from:
-    #initiate
-    def __init__(self,url,item, itemtype="div"):
-        self.__url = url
-        self.__item = item
-        self.__itemtype = itemtype
-        return
-    def __call__(self):
-        return self._getitemvalue_()
-    def __str__(self):
-        return str("Адрес объекта: "+self.__url+" ; ищем <"+self.__itemtype+" class='"+ self.__item+"'>")
-    def _getitemvalue_(self):
-        value = [""]
-        page = urllib.request.urlopen(self.__url)
-        page.getcode()
-        if page.getcode() == 200:
-            text = page.read()
-            soup = BeautifulSoup(text,features="html.parser")
-            value = soup.findAll(self.__itemtype, class_=self.__item)
-        else:
-            value = ["go duck youself"]
-        return value
-    pass
-#==================================================================================================================================
-class preprocess_data:
-    '''
-    обработать то, что выдает get_data_from
-    '''
-    def __init__(self, position):
-        self.__position = position
-        return
-    def __str__(self):
-        return ""
-    def __repr__(self):
-        return self.__str()
-    def __call__(self,inputlist):
-        return self._do_preprocess_(inputlist)
-    def _do_preprocess_(self, inputlist):
-        output = ""
-        position = self.__position
-        if(position < 0):
-            output = (',').join(inputlist)
-        if(position < len(inputlist)):
-            output = inputlist[position].text
-        else:
-            output = inputlist[-1]
-            output = output.text
-        return output
     pass
 #==================================================================================================================================
 class ultra_strip:
@@ -116,21 +91,113 @@ class ultra_strip:
         return result
     pass
 #==================================================================================================================================
-# тесты модуля
-#def _tests():
-#    rbstr = ColoredStr("red, bold")
-#    gbstr = ColoredStr("green, bold")
-#    failed = rbstr("!!!FAILED!!! : ")
-#    passed = gbstr("PASSED : ")
-#    htc = HandmadeTestDecorator(failed, passed)
-#    print("begin testing")
-#    print(htc(test_webpage_str_1)())
-#    print(htc(test_webpage_str_2)())
-#    print(htc(test_webpage_open_1)())   
-#    print("testing end")
-#    return None
+class page_element:
+    #initiate
+    #--------------------------------------------------------------------------------
+    def __init__(self, item_alias: str, item_name:str, item_type:str, item_num:int, stripper:ultra_strip) -> str:
+        self.__item_alias = item_alias
+        self.__item_type = item_type
+        self.__item_name = item_name  
+        self.__item_num = item_num 
+        self.__stripper = stripper
+    #--------------------------------------------------------------------------------
+    @property
+    def item_alias(self):
+        return self.__item_alias
+    #--------------------------------------------------------------------------------
+    def __call__(self, soup):
+        dirty_value = self._getitemvalues_(soup)
+        llog(dirty_value)
+        clean_value = self.__stripper(dirty_value[self.__item_num].text) 
+        llog(clean_value)
+        return clean_value
+    #--------------------------------------------------------------------------------
+    def __str__(self):
+        return str("Ищем "+self.__item_alias +" <"+self.__item_type+" class='"+ self.__item_name+"'>. под номером "+str(self.__item_num))
+    #--------------------------------------------------------------------------------
+    def __repr__(self):
+        return self.__str__()
+    #--------------------------------------------------------------------------------
+    def _getitemvalues_(self, soup) -> str:
+        value = ['']
+        llog('_getitemvalues_, <%s class=%s>' %(self.__item_type, self.__item_name))
+        value = soup.findAll(self.__item_type, class_=self.__item_name)
+        return value
+    #--------------------------------------------------------------------------------
+    pass
+#==================================================================================================================================
+class product_info:
+    """
+    Класс, ответственный за получение корректных данных с одной веб странички
+    должен предоставлять интерфейс к заполнению полей, которые потом нужно будет выдрать.
+    должен возвращать словарь с именованными данными.
+    """
+    #------------------------------------------------------------------------------------
+    def __init__(self, url:str, elements:dict):
+        """
+        инициализируется урлом, с которого нужно забирать данные.
+        """
+        self.__url = url
+        self.__page = webpage(self.__url) #сразу докинем читалку страниц
+        self.__soup = None
+        self.__data_loaded = False
+        self.__elements = []
+        self.setup(elements)
+    #------------------------------------------------------------------------------------
+    def __str__(self):
+        return ('\n').join((str(element) for element in self.__elements))
+    #------------------------------------------------------------------------------------
+    def setup(self, elements:dict):
+        """
+        метод, принимающий и ининциализирующий параметры, которые неободимо выдрать из загруженного супа
+        elements должен иметь структуру "класс элемента": ("тип элемента",номер_элемента, стриппер), например 
+        {'a_price_element':('div',0,a_price_element_stripper)}
+        
+        ИЗЛИШНЕ ПОКА ЭТОТ РАЗБОР ДРАМАТИЗИРОВАТЬ НЕ БУДУ, ПО ХОДУ ДЕЛА ПРИДЕТСЯ ДОРАБОТАТЬ
+        """
+        self.__elements = []
+        for key in elements.keys():
+            self.__elements.append(page_element(key,elements[key][0],elements[key][1],elements[key][2],elements[key][3]))
+        
+    #------------------------------------------------------------------------------------
+    def load_page(self):
+        """
+        Долгий метод, будет загружать страницу и отдавать её в суп.
+        на выходе либо заполнит суп данными, либо не заполнит
+        """
+        self.__page.open()
+        if(self.__page.is_open()):
+            self.__data_loaded = True
+            self.__soup = BeautifulSoup(self.__page.text,features="html.parser") 
+        else:
+            self.__data_loaded = False
+    #------------------------------------------------------------------------------------ 
+    def get_all(self):
+        result = {}
+        if(self.__data_loaded):
+            for element in self.__elements:
+                result[element.item_alias] = element(self.__soup)
+        else:
+            pass #TODO хз, может и просто надо удалить
+        return result 
+                                   
+    #------------------------------------------------------------------------------------                               
+    pass
+
 #==================================================================================================================================
 def main():
+    #llog = LocalLog(True)
+    superstripper = ultra_strip([u" ",u"\u20bd",u"\xa0"])
+    nostrip = ultra_strip([])
+    smara1_data_2_get = {"Цена":("catalog-detail__price","div", 0, superstripper),
+                  "Название товара":("catalog-detail__name","h1", 0, nostrip)
+                  }
+    smara1 = product_info("https://sport-marafon.ru/catalog/gamaki/gamak-eno-doublenest-print-tie-dye-red/",smara1_data_2_get)
+    smara1.load_page()
+    data = smara1.get_all()
+    print(data)
+                                                                                                       
+    
     return None
 #==================================================================================================================================
 if __name__ == '__main__':
--- users/sivanov/lessons/lesson_21_hw/parsers/test_getdatafrom.py
+++ users/sivanov/lessons/lesson_21_hw/parsers/test_getdatafrom.py
@@ -1,27 +1,101 @@
 #!/usr/local/bin/python
 # coding: utf-8
-from getdatafrom import webpage
+"""
+Тесты для модуля getddatafrom
+"""
 import os
+from getdatafrom import webpage
+from getdatafrom import ultra_strip
+from getdatafrom import get_element
 #тесты
 def test_webpage_str_1():
-    assert str(webpage('test')) == 'url: test, opened: False, error code: 0'
-    return None
+    """
+    Тест создания экземпляра webpage и работоспособности его метода __str__
+    """
+    assert str(webpage('test')) == 'url: test, opened: False, , error codes: HTTP: 0, URL: 0'
+#--------------------------------------------------------------------------------------------------
 def test_webpage_str_2():
-    assert str(webpage('test')) != 'url: test, opened: True, error code: 0'
-    return None
+    """
+    Тест создания экземпляра webpage и работоспособности его метода __str__
+    """
+    assert str(webpage('test')) != 'url: test, opened: True, , error codes: HTTP: 0, URL: 0'
+#--------------------------------------------------------------------------------------------------
+def test_webpage_last_url_error_1():
+    """
+    Тест создания экземпляра webpage и свойства last_URL_error_code
+    """
+    assert webpage('test').last_URL_error_code == 0
+#--------------------------------------------------------------------------------------------------
+def test_webpage_last_http_error_1():
+    """
+    Тест создания экземпляра webpage и свойства last_URL_error_code
+    """
+    assert webpage('test').last_HTTP_error_code == 0
+#--------------------------------------------------------------------------------------------------
+def test_webpage_last_error_slow():
+    """
+    ТЕСТ МЕДЛЕННЫЙ, ходит в интернет
+    Тест создания экземпляра webpage, его методов open(), is_open() и свойства last_URL_error_code
+    """
+    a_page = webpage('http://moscompass.ru')
+    a_page.open()
+    assert a_page.is_open()
+    assert a_page.last_URL_error_code == 200
+#--------------------------------------------------------------------------------------------------
+def test_webpage_last_error_slow_2():
+    """
+    ТЕСТ МЕДЛЕННЫЙ, ходит в интернет
+    Тест создания экземпляра webpage, его методов open(), is_open() и свойства last_HTTP_error_code
+    """
+    a_page = webpage('http://moscompass.ru/notexists.html')
+    a_page.open()
+    assert not a_page.is_open()
+    assert a_page.last_HTTP_error_code == 404
+#--------------------------------------------------------------------------------------------------
 def test_webpage_open_1():
-    test_html = '<html><head><meta charset="utf-8"></head><body><div class=\"test\">some text</div><div class=\"price\">1 345р.</div></body></html>'
+    """
+    ТЕСТ СОЗДАЕТ ФАЙЛ В ТЕКУЩЕМ КАТАЛОГЕ И ПОТОМ УДАЛЯЕТ ЕГО
+    Тест создания экземпляра webpage, его методов open(), is_open()
+    """
+    test_html =  '<html><head><meta charset="utf-8"></head><body>'
+    test_html += '<div class=\"test\">some text</div>'
+    test_html += '<div class=\"price\">1 345р.</div></body></html>'
     filename = os.getcwd() + '/test.html'
-    assert (not os.path.exists(filename)) 
-    with open(filename,'w') as f:
-        f.write(test_html)
-    test_page = webpage('file://' + filename)
-    
-    text = test_page.open().decode('utf-8')
+    assert not os.path.exists(filename)
+    with open(filename,'w',encoding='utf-8') as output_file:
+        output_file.write(test_html)
+    a_page = webpage('file://' + filename)
+    text = a_page.open().decode('utf-8')
     os.remove(filename)
+    assert a_page.is_open()
     assert text == test_html# + 'fail'
-    
-    return None
-#--------------------------------------------------------------------------------------------------------
-    
+#--------------------------------------------------------------------------------------------------
+def test_ultra_stripper_1():
+    """
+    тест работы стриппера - очистка строки от нежелательных символов
+    """
+    inputdata = '6 500 ₽    '
+    superstripper = ultra_strip([' ','\u20bd','\xa0'])
+    assert superstripper(inputdata) == '6500'
+#--------------------------------------------------------------------------------------------------
+def test_ultra_stripper_2():
+    """
+    тест работы стриппера - метода __str__
+    """
+    superstripper = ultra_strip([' ','\u20bd','\xa0'])
+    assert str(superstripper) == ' ,\u20bd,\xa0'
+#--------------------------------------------------------------------------------------------------
+def test_ultra_stripper_3():
+    """
+    тест работы стриппера - очистка строки от нежелательных символов
+    """
+    inputdata = 'h1e2l3l4o5 6w7o8r9l0d'
+    superstripper = ultra_strip(list('hello world'))
+    assert superstripper(inputdata) == '1234567890'
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
     
