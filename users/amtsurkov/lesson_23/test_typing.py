1dbabfb476fa96c9dbee33ae47a555284a48be0c
amtsu <jupyter-amtsu@vm6.template>
2022-12-11 20:50:57 +0300

    black check

--- users/amtsurkov/lesson_23/test_typing.py
+++ users/amtsurkov/lesson_23/test_typing.py
@@ -1,17 +1,20 @@
-#def calculate_sum(price: int, count: int) -> int:
-#def calculate_sum(price, count) -> int:
+# def calculate_sum(price: int, count: int) -> int:
+# def calculate_sum(price, count) -> int:
 def calculate_sum(price, count) -> str:
     return price * count
 
+
 final_price = calculate_sum(1000, 3)
 
-#final_price_2  = calculate_sum(80, 4) 
-final_price_2  = calculate_sum('Moloko', 4) 
+# final_price_2  = calculate_sum(80, 4)
+final_price_2 = calculate_sum("Moloko", 4)
+
 
 def test_2(long_string: str):
     print(long_string)
 
-test_2(calculate_sum('Moloko', 2))
+
+test_2(calculate_sum("Moloko", 2))
 
 test_2(final_price)
 
