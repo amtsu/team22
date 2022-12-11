58582f150b6427c74d6d78ab1e723d774e86c4f6
Serg (sivanov) <isergy@mail.ru>
2022-12-12 01:05:25 +0300

    renameing

--- users/sivanov/src/dags/dag_sivanov_do_parse_22.py
+++ users/sivanov/src/dags/dag_sivanov_do_parse_22.py
@@ -1,28 +1,40 @@
 """
 модуль для airflow, запускает разбор сайтов и выгрузку их в бд.
 """
+import sys
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+
+
 # прописать путь к коду с уроками
-import sys
-if "/home/jupyter-isergy/github/team22/users/sivanov/lessons/lesson_22_hw/parsers/" not in sys.path:
-    sys.path.append("/home/jupyter-isergy/github/team22/users/sivanov/lessons/lesson_22_hw/parsers/")
+if (
+    "/home/jupyter-isergy/github/team22/users/sivanov/lessons/lesson_22_hw/parsers/"
+    not in sys.path
+):
+    sys.path.append(
+        "/home/jupyter-isergy/github/team22/users/sivanov/lessons/lesson_22_hw/parsers/"
+    )
 from do_parse import parser
 
 # вот тут идет управление зданием эйрфлоу
 # про расписание смотри тут https://en.wikipedia.org/wiki/Cron
 # про datetime наверное тут https://pendulum.eustace.io/
-with DAG(dag_id="saivanov_parse_22", start_date=datetime(2022, 12, 11), schedule="0 0 * * *") as dag:
+with DAG(
+    dag_id="saivanov_parse_22", start_date=datetime(2022, 12, 2), schedule="0 0 * * *"
+) as dag:
 
     start = BashOperator(task_id="start", bash_command="echo sivanov_parse_22")
 
     @task()
     def airflow():
+        """
+        парсим, выливаем в джанго
+        """
         print("start sivanov_parse_22")
-        #начинаем парсить сайты
-        result = parser.do_parse() # пока это всё что могу
-        #отправить в джанго
+        # начинаем парсить сайты
+        result = parser.do_parse()  # пока это всё что могу
+        # отправить в джанго
         parser.send_to_api(result)
         print("end sivanov_parse_22")
 
@@ -30,10 +42,9 @@ with DAG(dag_id="saivanov_parse_22", start_date=datetime(2022, 12, 11), schedule
 
     # Set dependencies between tasks
     start >> airflow() >> end
-#====================================================================================================
-#def main():
+# =============================================================================================
+# def main():
 #    result = parser.do_parse()
 #    parser.send_to_api(result)
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    main()
-    
