8cc561da7e9642a8c28fe99390ad5cc1e18944ae
Serg (sivanov) <isergy@mail.ru>
2022-12-18 17:27:48 +0300

    finished pylinting of dag_sivanov_do_parse_22.py

--- users/sivanov/lessons/lesson_22_hw/dag_sivanov_do_parse_22.py
+++ users/sivanov/lessons/lesson_22_hw/dag_sivanov_do_parse_22.py
@@ -2,20 +2,22 @@
 модуль для airflow, запускает разбор сайтов и выгрузку их в бд.
 """
 from datetime import datetime
-import sys
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+
 # это впрочем тоже космический костыль
-import users.sivanov.dags.parsers
-from users.sivanov.dags.parsers.do_parse import parser
+# import users.sivanov.dags.parsers
+from users.sivanov.d221214t1844.parsers.do_parse import Parser  # type: ignore  #pylint: disable=E0401
 
 # вот тут идет управление зданием эйрфлоу
 # про расписание смотри тут https://en.wikipedia.org/wiki/Cron
 # про datetime наверное тут https://pendulum.eustace.io/
 with DAG(
-    dag_id="sivanov_parse_22", start_date=datetime(2022, 12, 9), schedule="0 0 * * *",
-    tags=["sivanov","parse_22"]
+    dag_id="sivanov_parse_22",
+    start_date=datetime(2022, 12, 9),
+    schedule="0 0 * * *",
+    tags=["sivanov", "parse_22"],
 ) as dag:
 
     start = BashOperator(task_id="start", bash_command="echo sivanov_parse_22")
@@ -27,15 +29,15 @@ with DAG(
         """
         print("start sivanov_parse_22")
         # начинаем парсить сайты
-        result = parser.do_parse()  # пока это всё что могу
+        result = Parser.do_parse()  # пока это всё что могу
         # отправить в джанго
-        #parser.send_to_api(result)
+        Parser.send_to_api(result)
         print("end sivanov_parse_22")
 
     end = BashOperator(task_id="end", bash_command="echo sivanov_parse_22")
 
     # Set dependencies between tasks
-    start >> airflow() >> end
+    start >> airflow() >> end # pylint: disable=W0106
 # =============================================================================================
 # def main():
 #    result = parser.do_parse()
