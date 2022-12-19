bb219ec2db96947983bd8e8767973612be1e684c
Serg (sivanov) <isergy@mail.ru>
2022-12-19 18:51:57 +0300

    added link to bot module documentation and a module with a functions (think it`ll be useful)

---
+++ users/sivanov/projects/bot/functions.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# coding: utf-8
+"""
+набор разных функций для бота
+"""
+from telebot import types
+
+
+def message_handler(message):
+    print("message comes, i handle it")
--- users/sivanov/projects/bot/main.py
+++ users/sivanov/projects/bot/main.py
@@ -5,15 +5,19 @@
 это модуль с тестом бота для телеги
 мануал взял вот тут:
 https://otus.ru/journal/bot-v-telegram-na-pitone-ot-a-do-ya/
+описание модуля тут
+https://pypi.org/project/pyTelegramBotAPI
 """
 from datetime import datetime
 import telebot
 from telebot import types
+#from functions import message_handler
 
 bot = telebot.TeleBot("5949080313:AAHE8ikdvHc5q30R1EeFa2se-j9F5dqokoo")
 # ========================================================================
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
+    #message_handler(message)
     if message.chat.type == "private":
         with open("bot.log", "a", encoding="utf-8") as fout:
             print(
@@ -23,22 +27,22 @@ def get_text_messages(message):
         print(f"message from {message.from_user.id} comes.")
 
         if message.text.lower() == "привет":
-            #bot.send_message(message.from_user.id, "Привет пидор.")
+            # bot.send_message(message.from_user.id, "Привет пидор.")
             bot.reply_to(message, "Привет пидор.")
-        #elif message.text == "/help":
+        # elif message.text == "/help":
         #    bot.send_message(message.from_user.id, "Напиши Привет")
-        #else:
+        # else:
         #    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-    elif message.chat.type == "group" or message.chat.type== "supergroup":
-        #print("group chat message")
+    elif message.chat.type == "group" or message.chat.type == "supergroup":
+        # print("group chat message")
         with open("bot.log", "a", encoding="utf-8") as fout:
             print(
-                   f"{datetime.now()}: FROM {message.from_user.id} in {message.chat.id} comes text: {message.text}",
-                    file=fout,
+                f"{datetime.now()}: FROM {message.from_user.id} in {message.chat.id} comes text: {message.text}",
+                file=fout,
             )
         if message.text.lower() == "привет":
-            bot.reply_to(message, "Привет пидор.")  
-        print (f'group message in {message.chat.id}')
+            bot.reply_to(message, "Привет пидор.")
+        print(f"group message in {message.chat.id}")
     else:
         print("unknown message")
     return None
