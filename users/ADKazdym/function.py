688cde0b80937080d6b53e813ff0df5d70622ca5
ADKazdym <vdnx818@gmail.com>
2023-01-30 13:48:03 +0300

    занятие с сержем 2

---
+++ users/ADKazdym/function.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+def double_string(string):
+    return string * 2
+
+def double_string1(string):
+    return string + " " + string
+
+def arithmetic(a,b,z):
+    if z=="+":
+        return a+b
+    elif z=="-":
+        return a-b
+    elif z=="*":
+        return a*b
+    elif z=="/":
+        return a/b
+    else:
+        return ("Неизвестная операция")
+
+def a(x, y):
+    return x**2 + y**2
+
+def c(a,b):
+    return (a**2 + b**2)**0.5
+
+def square_parameters(a):
+    return 4*a , a**2 , a*(2**0.5)
