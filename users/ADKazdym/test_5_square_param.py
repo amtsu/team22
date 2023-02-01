b3126e38cae70aed448ac39099c2f63d5a10db14
ADKazdym <vdnx818@gmail.com>
2023-02-01 10:38:23 +0300

    dodelal test square param

--- users/ADKazdym/test_5_square_param.py
+++ users/ADKazdym/test_5_square_param.py
@@ -1,5 +1,9 @@
 from function import square_parameters
 def test_1_square_parameters():
-    assert square_parameters(1)==4 and 1 and 1.4142135623730951 
+    a=(20, 25, 7.0710678118654755)
+    square_parameters(5)==(20, 25, 7.0710678118654755)
+    assert square_parameters(5)==a 
 def test_2_square_parameters():
-    assert not square_parameters(1)==1 and 2 and  3
+    square_parameters(5)==(20, 25, 7.0710678118654755)
+    a=(20, 25, 7.0710678118654755)
+    assert not square_parameters(5)!=a
