dcc84fbd5a31d2c69fa6c6f1288d59f919cc6d2a
Serg (sivanov) <isergy@mail.ru>
2023-02-03 18:45:56 +0300

    добавил сортировку вставками

--- users/sivanov/lessons/lesson_24_sorting/test_selectionsort.py
+++ users/sivanov/lessons/lesson_24_sorting/test_selectionsort.py
@@ -1,40 +1,40 @@
 #!/usr/local/bin/python
 # coding: utf-8
 """
-модуль тестирования для сортировки вставками
+модуль тестирования для сортировки выбором
 попробую быстренько слабать
 """
-from insertionsort import(
-    insertion_sort,
+from selectionsort import(
+    selection_sort,
 )
-def test_insertion_sort_1():
+def test_selection_sort_1():
     """
     один из тестов сортировки
     """
-    assert insertion_sort([1,0])==[0,1]
+    assert selection_sort([1,0])==[0,1]
     
-def test_insertion_sort_2():
+def test_selection_sort_2():
     """
     один из тестов сортировки
     """
-    assert insertion_sort([3,2,1]) == [1,2,3]
+    assert selection_sort([3,2,1]) == [1,2,3]
     
-def test_insertion_sort_3():
+def test_selection_sort_3():
     """
     один из тестов сортировки
     """
-    assert insertion_sort([5]) == [5]
+    assert selection_sort([5]) == [5]
 
-def test_insertion_sort_4():
+def test_selection_sort_4():
     """
     один из тестов сортировки
     """
-    assert insertion_sort([5,5,3,4,2,2,1,5]) == [1,2,2,3,4,5,5,5]
+    assert selection_sort([5,5,3,4,2,2,1,5]) == [1,2,2,3,4,5,5,5]
 
-def test_insertion_sort_5():
+def test_selection_sort_5():
     """
     один из тестов сортировки
     """
     given   =  [14, 8, 14, 15, 8, 5, 2, 2, 10, 10, 7, 9, 15, 10, 11]
     expected = [2, 2, 5, 7, 8, 8, 9, 10, 10, 10, 11, 14, 14, 15, 15]
-    assert insertion_sort(given) == expected 
+    assert selection_sort(given) == expected 
