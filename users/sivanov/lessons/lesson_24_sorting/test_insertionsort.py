8d9f2a25b1da6c34df924e5506a77318585d5884
Serg <109685254+isergy@users.noreply.github.com>
2023-02-05 20:20:00 +0300

    sivanov ветка про дз про алгоритмы сортировки (#87)
    * started working with handfritten sorting functions. bubble sort almost done
    
    * добавил немного визуализации и тестов для пузырька
    
    * забыл тесты сохранить перед коммитом лол
    
    * n
    
    * добавил сортировку вставками, tdd это кайф. исправил грамматическую ошибку в баблсорте
    
    * добавил сортировку вставками
    
    * добавил быструю сортировку

---
+++ users/sivanov/lessons/lesson_24_sorting/test_insertionsort.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python
+# coding: utf-8
+"""
+модуль тестирования для сортировки вставками
+попробую быстренько слабать
+"""
+from insertionsort import(
+    insertion_sort,
+)
+def test_insertion_sort_1():
+    """
+    один из тестов сортировки
+    """
+    assert insertion_sort([1,0])==[0,1]
+    
+def test_insertion_sort_2():
+    """
+    один из тестов сортировки
+    """
+    assert insertion_sort([3,2,1]) == [1,2,3]
+    
+def test_insertion_sort_3():
+    """
+    один из тестов сортировки
+    """
+    assert insertion_sort([5]) == [5]
+
+def test_insertion_sort_4():
+    """
+    один из тестов сортировки
+    """
+    assert insertion_sort([5,5,3,4,2,2,1,5]) == [1,2,2,3,4,5,5,5]
+
+def test_insertion_sort_5():
+    """
+    один из тестов сортировки
+    """
+    given   =  [14, 8, 14, 15, 8, 5, 2, 2, 10, 10, 7, 9, 15, 10, 11]
+    expected = [2, 2, 5, 7, 8, 8, 9, 10, 10, 10, 11, 14, 14, 15, 15]
+    assert insertion_sort(given) == expected 
