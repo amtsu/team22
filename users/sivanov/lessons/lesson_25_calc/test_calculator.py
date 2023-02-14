0a2da06b80b7d341780fd9d7c48e4a24147216e9
Serg (sivanov) <isergy@mail.ru>
2023-02-14 16:13:56 +0300

    добавил тесты

---
+++ users/sivanov/lessons/lesson_25_calc/test_calculator.py
@@ -0,0 +1,108 @@
+"""
+файл с описанием тестов класса calculator
+"""
+import pytest
+import calculator
+
+
+def test_calculator_1():
+    """
+    один из тестов  класса калькулятор
+    простое вычитание
+    """
+    calc = calculator.Calculator()
+    assert calc("1-4") == -3
+
+
+def test_calculator_2():
+    """
+    один из тестов  класса калькулятор
+    вычитание с унарным оператором в качестве
+    первого аргумента
+    """
+    calc = calculator.Calculator()
+    assert calc("-1-4") == -5
+
+
+def test_calculator_3():
+    """
+    один из тестов  класса калькулятор
+    вычитание двух унарных операторов
+    """
+    calc = calculator.Calculator()
+    assert calc("-1 - -4") == 3
+
+
+def test_calculator_4():
+    """
+    один из тестов  класса калькулятор
+    деление
+    """
+    calc = calculator.Calculator()
+    assert calc("1/4") == 0.25
+
+
+def test_calculator_5():
+    """
+    один из тестов  класса калькулятор
+    умножение на унарный оператор
+    """
+    calc = calculator.Calculator()
+    assert calc("5*(-4.1)") == -20.5
+
+
+def test_calculator_6():
+    """
+    один из тестов  класса калькулятор
+    """
+    calc = calculator.Calculator()
+    with pytest.raises(AssertionError):
+        calc("5*()")
+
+
+def test_calculator_7():
+    """
+    один из тестов  класса калькулятор
+    некорректные входные данные, assertion error
+    """
+    calc = calculator.Calculator()
+    with pytest.raises(AssertionError):
+        calc("5*6+7")
+
+
+def test_calculator_8():
+    """
+    один из тестов  класса калькулятор
+    деление унарного оператора на 0, zero division error
+    """
+    calc = calculator.Calculator()
+    with pytest.raises(ZeroDivisionError):
+        calc("-5/0")
+
+
+def test_calculator_9():
+    """
+    один из тестов  класса калькулятор
+    унарный оператор от плохого операнда, type error
+    """
+    calc = calculator.Calculator()
+    with pytest.raises(TypeError):
+        calc("-'fuck' * 5")
+
+
+def test_calculator_10():
+    """
+    один из тестов  класса калькулятор
+    умножение строки на число
+    """
+    calc = calculator.Calculator()
+    assert calc("'duck ' * 5") == "duck duck duck duck duck "
+
+
+def test_calculator_11():
+    """
+    один из тестов  класса калькулятор
+    умножение строки на отрицательное число, лол кек
+    """
+    calc = calculator.Calculator()
+    assert calc("'fuck ' * -5") == ""
