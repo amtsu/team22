429078dd091cba89dd9a355822dcded1d8992065
Serg (sivanov) <isergy@mail.ru>
2023-02-14 15:42:21 +0300

    первая ревизия калькулятора, до тестов

--- users/sivanov/lessons/lesson_25_calc/calculator.py
+++ users/sivanov/lessons/lesson_25_calc/calculator.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 файл с описанием класса калькулятор для вычисления простых бинарных операторов
 https://stackoverflow.com/questions/15197673/using-pythons-eval-vs-ast-literal-eval
@@ -8,19 +9,31 @@ import ast
 import operator
 
 
+=======
+import ast, operator
+>>>>>>> первая ревизия калькулятора, до тестов
 class Calculator:
     """
     Класс калькулятор, инициализируется строкой с простым выражением типа
     const operator const, где const - любое числовое выражение,
+<<<<<<< HEAD
     а operator это одна из операций : сложение, вычитание,
     деление или умножение
     """
 
+=======
+    а operator это одна из операций : сложение, вычитание, деление или умножение
+    https://stackoverflow.com/questions/15197673/using-pythons-eval-vs-ast-literal-eval  - 
+    тут есть готовый более универсального вычислителя, блин,
+    обидно, искал инфу как работает нашел готовое решение посередине поисков
+    """
+>>>>>>> первая ревизия калькулятора, до тестов
     def __init__(self):
         """
         инициализация
         создаем списки необходимых операций
         """
+<<<<<<< HEAD
         self.__bin_ops = {  # операции с двумя параметрами
             ast.Add: operator.add,
             ast.Sub: operator.sub,
@@ -31,11 +44,24 @@ class Calculator:
             ast.USub: operator.neg,
             ast.UAdd: operator.pos,
         }
+=======
+        self.__bin_ops = { # операции с двумя параметрами
+            ast.Add  : operator.add,
+            ast.Sub  : operator.sub,
+            ast.Mult : operator.mul,
+            ast.Div  : operator.truediv,
+            }
+        self.un_ops = { # операции с одним параметром
+            ast.USub : operator.neg,
+            ast.UAdd : operator.pos,
+            }
+>>>>>>> первая ревизия калькулятора, до тестов
 
     def __call__(self, operation):
         """
         функция, делающая проверку корректности строки на входе и ее выполнение
         """
+<<<<<<< HEAD
         value = ast.parse(operation, mode="eval")
         assert isinstance(value.body, (ast.BinOp))  # ожидаем бинарный операнд
         assert isinstance(
@@ -76,3 +102,25 @@ class Calculator:
 if __name__ == "__main__":
     do_calc = Calculator()
     print(f"результат выполнения (1-4) = {do_calc('1-4')}")
+=======
+        value = ast.parse(operation, mode = 'eval')
+        assert isinstance(value.body,(ast.BinOp)) # ожидаем бинарный операнд
+        assert isinstance(value.body.left,(ast.Constant, ast.UnaryOp)) #проверяем левый аргумент
+        assert isinstance(value.body.right,(ast.Constant, ast.UnaryOp)) #проверяем правый аргумент
+        assert isinstance(value.body.op, tuple(self.__bin_ops)) # проверяем корректность операнда
+        if (isinstance(value.body.left,ast.UnaryOp)):
+            assert(isinstance(value.body.left.operand, ast.Constant)) # проверяем корректность левого аргумента
+            self.__left = self.un_ops[type(value.body.left.op)](value.body.left.operand.value)
+        else:
+            self.__left = value.body.left.value
+        if (isinstance(value.body.right,ast.UnaryOp)):
+            assert(isinstance(value.body.right.operand, ast.Constant)) # проверяем корректность правого аргумента
+            self.__right = self.un_ops[type(value.body.right.op)](value.body.right.operand.value)
+        else:
+            self.__right = value.body.right.value
+        return self.__bin_ops[type(value.body.op)](self.__left, self.__right) # пытаемся вычислить результат
+
+if __name__ == "__main__":
+   do_calc = Calculator()
+   print(f"результат выполнения (1-4) = {do_calc('1-4')}")
+>>>>>>> первая ревизия калькулятора, до тестов
