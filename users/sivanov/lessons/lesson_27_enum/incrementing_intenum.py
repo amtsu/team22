ca8c8c44359dbfdd38358e9a16e75b912524025a
Serg (sivanov) <isergy@mail.ru>
2023-03-14 01:46:35 +0300

    сравнение enum.Enum и enum.IntEnum. всё, я устал

--- users/sivanov/lessons/lesson_27_enum/incrementing_intenum.py
+++ users/sivanov/lessons/lesson_27_enum/incrementing_intenum.py
@@ -9,7 +9,7 @@ import enum
 
 
 @enum.unique  # не пропустит одинаковых значений
-class Numbers(enum.Enum):
+class Numbers(enum.IntEnum):
     """
     просто именованные константы, которые можно складывать и сравнивать
     """
@@ -26,24 +26,7 @@ class Numbers(enum.Enum):
     NINE = enum.auto()
     TEN = enum.auto()
     # ZEROO = 0 # с декоратором @enum.unique такое не пройдёт, если уже есть ZERO
-    def __add__(self, other):
-        """
-        операция +. 
-        наверное, можно как-то по-другому это написать
-        """
-        if self.__class__ is other.__class__:
-            # если выходит за границы, падает
-            # нужно поправить            
-            return Numbers(self.value + other.value)
-        return NotImplemented    
-    def __gt__(self, other):
-        """
-        gt это операция >
-        нужно описать ещё ge, le и lt
-        """
-        if(self.__class__ is other.__class__):
-            return self.value > other.value
-        return NotImplemented
+      
 # ==============================================================================
 
 
@@ -67,13 +50,13 @@ def main():
     else:
         print(f"var_one is greater than var_two wtf???")
     print(f"var_one + var_two = {(var_one + var_two)}")
-    print("вот это - особенность Enum, получили Numbers.THREE")
+    print("вот это - особенность IntEnum, так как ожидалось Numbers.THREE")
     var_six = Numbers.SIX
-    #print(f"var_six + 1 is {var_six + 1}")
+    print(f"var_six + 1 is {var_six + 1}")
     print(f"Numbers(3) is {Numbers(3)}")
-    #print(f"[overflow] var_six + var_six = {(var_six + var_six)}")
-    print("Но - корректно побороть выход за зону доступных enum мне не удалось,")
-    print("то есть Numbers(6) + numbers(7) должно закончится чем-то логичным")
+    print(f"[overflow] var_six + var_six = {(var_six + var_six)}, тип результата: {type(var_six + var_six)}")
+    print("Результат математических операций над наследниками IntEnum - это int")
+        
 # ===============================================================================
 if __name__ == "__main__":
     main()
