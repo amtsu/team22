646d9f7e9863290202f94a878558ceda29ccd7ce
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-03-31 12:01:48 +0300

    Доработанные функции с 3 разными методами добавления

--- users/EANabatov/work_with_files/work_with_txt.py
+++ users/EANabatov/work_with_files/work_with_txt.py
@@ -10,9 +10,9 @@ def print_file(file_name: str, number_of_characters_to_read: int) -> str:
     file_descriptor.close()
 
 
-def add_in_file(file_name: str, adding_name: str):
+def add_in_file_1_method(file_name: str, adding_name: str):
     """
-    Добавляет в конец введенного файла введенную строку
+    Добавляет в конец введенного файла введенную строку с помощью ключа "a"
     :param file_name: file name in str
     :param adding_name: adding name in str
     """
@@ -21,6 +21,34 @@ def add_in_file(file_name: str, adding_name: str):
     file_descriptor.close()
 
 
+def add_in_file_2_method(file_name: str, adding_name: str):
+    """
+    Добавляет в конец файла введенную строку с помощью ключа "w+"
+    :param file_name: file name in str
+    :param adding_name: adding name in str
+    """
+    with open(file_name, "r+") as file:
+        file.seek(0, 2)
+        file.write(f" {adding_name}")
+
+
+def add_in_file_3_method(file_name: str, adding_name: str):
+    """
+    Добавляет в конец файла введенную строку используя ключи "r" и "w"
+    :param file_name:
+    :param adding_name:
+    """
+    with open(file_name, "r") as file:
+        kek = file.readline()  # Сохраняем в переменную, что бы потом при вызове файла в режиме записи,
+        # восстановить стертые фрагменты
+    with open(file_name, "w") as file:
+        file.write(f"{kek} {adding_name}")
+
+
 print_file("names.txt", 200)
-add_in_file("names.txt", "Evgeny")
+add_in_file_1_method("names.txt", "Viktor")
+print_file("names.txt", 300)
+add_in_file_2_method("names.txt", "Evgeny")
+print_file("names.txt", 300)
+add_in_file_3_method("names.txt", "Keker")
 print_file("names.txt", 300)
