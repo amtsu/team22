ba9bf7f3d298d25f414c92897415ad394864422a
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-07-17 23:57:38 +0300

    black/mypy для модуля

--- src/simple_model_project/EANabatov_module.py
+++ src/simple_model_project/EANabatov_module.py
@@ -4,32 +4,32 @@ from abstractmodule import AbstractModule
 
 
 class EANabatovModule(AbstractModule):
-    """ реализация абстрактного класса с использованием паттерна Singleton """
+    """реализация абстрактного класса с использованием паттерна Singleton"""
 
     def __new__(cls):
-        """ Реализация Singleton """
-        if not hasattr(cls, 'instance'):
+        """Реализация Singleton"""
+        if not hasattr(cls, "instance"):
             cls.instance = super(EANabatovModule, cls).__new__(cls)
         return cls.instance
 
     def __init__(self):
-        """ Инициализация атрибутов класса """
+        """Инициализация атрибутов класса"""
         instance = None
 
     def name(self) -> str:
-        """ возвращает название класса """
+        """возвращает название класса"""
 
     def prepare(self) -> None:
-        """ Выполняется перед началом цикла вызова методов step """
+        """Выполняется перед началом цикла вызова методов step"""
 
     def step(self) -> Tuple[float, int]:
-        """ Возвращает состояние модуля на данном шаге,
-        выполняется на каждом шаге, пока разрешено """
+        """Возвращает состояние модуля на данном шаге,
+        выполняется на каждом шаге, пока разрешено"""
 
     def is_done(self) -> bool:
-        """ Сигнализирует о окончании процесса моделирования """
+        """Сигнализирует о окончании процесса моделирования"""
 
     def create_instance(self):
-        """ создает экземпляр класса и возвращает его в виде объекта """
+        """создает экземпляр класса и возвращает его в виде объекта"""
         nabatov_object = EANabatovModule()
         return nabatov_object.instance
