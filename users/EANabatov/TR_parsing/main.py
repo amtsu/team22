70030bf0840a50f22ed61e9ced297eb17f82a4c2
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-07-18 18:07:00 +0300

    какое то продолжение нащупал название и стоимость

--- users/EANabatov/TR_parsing/main.py
+++ users/EANabatov/TR_parsing/main.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-page = urllib.request.urlopen("https://www.moscowbooks.ru/book/1156180/")
-if page.getcode() == 200:
-    text = page.read()
+for index in range(1156290, 1156300):
+    try:
+        page = urllib.request.urlopen("https://www.moscowbooks.ru/book/" + str(index) + "/")
+        if page.getcode() == 200:
+            text = page.read()
+            soup = BeautifulSoup(text, features="html.parser")
+            print(page)
+            print(soup.find("h1"))
+            print(soup.find("span", class_= "rubs"))
+    except:
+        pass
 
-soup = BeautifulSoup(text, features="html.parser")
-list_tag_h1 = soup.findAll("h1")
-print(list_tag_h1[0].text)
-
+# soup = BeautifulSoup(text, features="html.parser")
+# list_tag_h1 = soup.findAll("h1")
+# print(list_tag_h1[0].text)
