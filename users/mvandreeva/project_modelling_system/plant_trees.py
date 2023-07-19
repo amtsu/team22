93e1b7d733bfb3057ae53c19445bc703f87114be
mvandreeva <Mariya.V.Andreeva@yandex.ru>
2023-07-20 00:54:25 +0300

    Add homework on modelling. Not sure it is correct.

---
+++ users/mvandreeva/project_modelling_system/plant_trees.py
@@ -0,0 +1,81 @@
+"""
+Класс для участия в моделирующей системе
+"""
+
+from typing import Tuple
+from abstractmodule import AbstractModule
+
+class PlantTrees(AbstractModule):
+    """
+    Класс, описываюший процесс посадки деревьев
+    При создвнии объекта класса необходимо передать параметры:
+    1) площадь посадки (кв.м)
+    2) кол-во деревьев для посадки
+    Итого, одно дерево занимает 4 кв.м
+    """
+    # Расстояние между деревьями в ряду должно быть не менее 3м,
+    # расстояние между рядами - не менее 5м
+
+    def __init__(self, area, trees_amount):
+        """
+        Конструктор
+        """
+        self.__area = area
+        self.__trees_amount = trees_amount
+        self.__planed_trees = 0
+        self.__trees_fit = 0
+
+    def name(self) -> str:
+        """
+        возвращает название модуля
+        """
+        module_name = str(self.__module__)
+        return module_name
+
+    def prepare(self) -> None:
+        """
+        Выполняется перед началом цикла вызова методов step
+        Определяет количество деревьев (<= self.__trees_amount),
+        которое может быть посажено на заданном участке площадью <=self.__area
+        """
+        # Предположим, одно дерево занимает 4 кв.м.
+        trees_to_area = self.__area / 4 #сколько деревьев можно посадить
+        if self.__trees_amount <= trees_to_area:
+            self.__trees_fit = self.__trees_amount
+        elif self.__trees_amount > trees_to_area:
+            self.__trees_fit = trees_to_area
+
+    def step(self) -> Tuple[float, int]:
+        """
+        Возвращает состояние модуля на данном шаге,
+        выполняется на каждом шаге, пока разрешено
+        возвращает значение засаженной площади,
+        кол-во посаженных деревьев на данном шаге
+        """
+        self.prepare()
+        step = 0
+        while self.__trees_fit:
+            self.__planed_trees +=1
+            self.__trees_fit -=1
+            step +=1
+        return (self.__planed_trees, step)
+
+    def is_done(self) -> bool:
+        """
+        Сигнализирует об окончании процесса моделирования
+        Критерии окончания посадки:
+        1) закончились деревья
+        2) закончилась площадь посадки
+        ?3) закончился рабочий день =) ?
+        """
+        is_done = False
+        if self.__trees_fit == 0:
+            print("Посадили, сколько смогли")
+            is_done = True
+        return is_done
+
+def createinstance(area, trees_amount) -> PlantTrees:
+    """
+    создает экземпляр класса PlantTrees
+    """
+    return PlantTrees(area, trees_amount)
---
+++ users/mvandreeva/project_modelling_system/test_plant_trees.py
@@ -0,0 +1,45 @@
+"""
+Тесты к модулю plant_trees.py
+"""
+import pytest
+from plant_trees import PlantTrees, createinstance
+
+def test_createinstance():
+    """
+    Тестирует функцию createinstance
+    """
+    trees = createinstance(300, 100)
+    type_trees = type(trees)
+    assert type_trees == PlantTrees
+
+def test_name():
+    """
+    Тестирует метод name
+    """
+    trees = createinstance(300, 100)
+    name = trees.name()
+    assert name == "plant_trees"
+
+def test_step():
+    """
+    Тестирует метод step
+    """
+    plant_trees = PlantTrees(50, 5)
+    # prepare_trees = plant_trees.prepare()
+    # trees_planted = prepare_trees.step()
+    # print(trees_planted)
+    # plant_trees.prepare()
+    trees_planted = plant_trees.step()
+    print(trees_planted)
+    assert trees_planted == 0
+
+def test_is_done():
+    """
+    Тестирует метод is_done
+    """
+    plant_trees = PlantTrees(10, 5)
+    # prepare_trees = plant_trees.prepare()
+    # trees_planted = prepare_trees.step()
+    plant_trees.step()
+    complete_plant = plant_trees.is_done()
+    assert complete_plant is True
