05477d9aa922592060d8cd08ecb8412de080f805
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-08-15 18:03:53 +0300

    получилось избавиться от лишнего html в тексте

--- users/EANabatov/TR_parsing/local_page_parser.py
+++ users/EANabatov/TR_parsing/local_page_parser.py
@@ -7,7 +7,7 @@ internet_price = []
 publisher = []
 info = []
 
-page = urllib.request.urlopen("/home/evgeniy/Documents/html/book_parsing.html")
+page = urllib.request.urlopen("file:///home/evgeniy/Documents/html/book_parsing.html")
 
 text = page.read()
 soup = BeautifulSoup(text, features="html.parser")
@@ -15,7 +15,7 @@ book_name.append(soup.find("h1"))  # название книги
 shop_price.append(soup.find("span", class_="rubs"))  # цена в магазине
 shop_price.append(soup.find("span", class_="silver rubs rubfont"))  # цена на сайте
 publisher.append(soup.find_all("dt", class_="book__details-value"))  # год и место издания
-info.append(soup.find_all("a"))  # издательство
+info.append(soup.find_all("dl", class_="book__details-item"))  # издательство
+info[0][0].text.split()
 
-print(publisher)
-print(info)
+# print(publisher)
