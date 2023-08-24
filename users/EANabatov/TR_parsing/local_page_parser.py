5f7f10b50f680a7b4712b4a49361cdc781a045d4
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-08-24 22:21:56 +0300

    добавил в словарь

--- users/EANabatov/TR_parsing/local_page_parser.py
+++ users/EANabatov/TR_parsing/local_page_parser.py
@@ -1,3 +1,5 @@
+from unicodedata import decimal
+
 from bs4 import BeautifulSoup
 import urllib.request
 
@@ -8,16 +10,27 @@ page = urllib.request.urlopen("file:///home/evgeniy/Documents/html/book_parsing.
 text = page.read()
 soup = BeautifulSoup(text, features="html.parser")
 
-book_name = soup.find("h1", class_="page-header__title").text.strip()  # название книги
-print(book_name)
-shop_price = soup.find("span", class_="rubs").text  # цена в магазине
-print(shop_price)
-internet_price = soup.find("span", class_="silver rubs rubfont").text  # цена на сайте
-print(internet_price)
-year = soup.find_all("dt", class_="book__details-value")[0].text.split()  # год и место издания
-print(year)
-publisher = soup.find_all("dl", class_="book__details-item")[0].text.split()
+# book_name_author_name = soup.find("h1", class_="page-header__title").text.strip().split(".")  # название книги
+book_name = soup.find("h1", class_="page-header__title").text.split(".")[0].strip()  # название книги
+print("book_name: ", book_name)
+summary["book_name"] = book_name
+author_name = soup.find("h1", class_="page-header__title").text.split(".")[1].strip()  # имя автора
+print("author_name: ", author_name)
+summary["author_name"] = author_name
+shop_price = soup.find("span", class_="rubs").text.strip()  # цена в магазине
+print("shop_price:", shop_price)
+summary["shop_price"] = shop_price
+internet_price = soup.find("span", class_="silver rubs rubfont").text.strip()  # цена на сайте
+print("internet_price:", internet_price)
+summary["internet_price"] = internet_price
+year = soup.find_all("dt", class_="book__details-value")[0].text.split()[0]  # год издания
+print("the_year_of_publishing:", year)
+summary["the_year_of_publishing"] = year
+publisher = soup.find_all("dl", class_="book__details-item")[0].text.split()[1]  # издатель
+print("publisher:", publisher)
+summary["publisher"] = publisher
 
+print(summary)
 # print(info[0][0].text.split())  # издательство
 
-print(publisher)
+
