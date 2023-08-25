0e985eec6361a41dccad555be76d277eb9c325dc
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-08-25 09:53:37 +0300

    локальная страница отпаршена

--- users/EANabatov/TR_parsing/local_page_parser.py
+++ users/EANabatov/TR_parsing/local_page_parser.py
@@ -5,32 +5,34 @@ import urllib.request
 
 summary = {}
 
-
 page = urllib.request.urlopen("file:///home/evgeniy/Documents/html/book_parsing.html")
 text = page.read()
 soup = BeautifulSoup(text, features="html.parser")
 
-# book_name_author_name = soup.find("h1", class_="page-header__title").text.strip().split(".")  # название книги
 book_name = soup.find("h1", class_="page-header__title").text.split(".")[0].strip()  # название книги
-print("book_name: ", book_name)
-summary["book_name"] = book_name
 author_name = soup.find("h1", class_="page-header__title").text.split(".")[1].strip()  # имя автора
-print("author_name: ", author_name)
-summary["author_name"] = author_name
-shop_price = soup.find("span", class_="rubs").text.strip()  # цена в магазине
-print("shop_price:", shop_price)
-summary["shop_price"] = shop_price
-internet_price = soup.find("span", class_="silver rubs rubfont").text.strip()  # цена на сайте
-print("internet_price:", internet_price)
-summary["internet_price"] = internet_price
-year = soup.find_all("dt", class_="book__details-value")[0].text.split()[0]  # год издания
-print("the_year_of_publishing:", year)
-summary["the_year_of_publishing"] = year
+shop_price = soup.find("span", class_="rubs").text.replace(" ","")  # цена в магазине
+internet_price = soup.find("span", class_="silver rubs rubfont").text.replace(" ","")  # цена на сайте
+year = soup.find_all("dt", class_="book__details-value")[1].text.split()[0]  # год издания
 publisher = soup.find_all("dl", class_="book__details-item")[0].text.split()[1]  # издатель
-print("publisher:", publisher)
+link = soup.find("link").get("href")  # ссылка на книгу
+pages = soup.find_all("dl", class_="book__details-item")[11].text.split()[1]  # количество страниц в книге
+product_code = soup.find_all("dl", class_="book__details-item")[12].text.split()[2]  # код товара
+vendor_code = soup.find_all("dl", class_="book__details-item")[13].text.split()[1]  # артикул
+isbn = soup.find_all("dl", class_="book__details-item")[14].text.split()[1]  # ISBN
+
+summary["book_name"] = book_name
+summary["author_name"] = author_name
+summary["shop_price"] = shop_price
+summary["internet_price"] = internet_price
+summary["the_year_of_publishing"] = year
 summary["publisher"] = publisher
+summary["link"] = link
+summary["pages"] = pages
+summary["product_code"] = product_code
+summary["vendor_code"] = vendor_code
+summary["isbn"] = isbn
+
 
 print(summary)
-# print(info[0][0].text.split())  # издательство
-
 
