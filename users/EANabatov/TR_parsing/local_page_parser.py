7326a0d1e107cecf137059584abe884ca7e7cce4
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-08-25 16:37:09 +0300

    пытаюсь сделать исключения пока не работает

--- users/EANabatov/TR_parsing/local_page_parser.py
+++ users/EANabatov/TR_parsing/local_page_parser.py
@@ -1,38 +1,44 @@
-from unicodedata import decimal
-
 from bs4 import BeautifulSoup
 import urllib.request
 
-summary = {}
 
-page = urllib.request.urlopen("file:///home/evgeniy/Documents/html/book_parsing.html")
-text = page.read()
-soup = BeautifulSoup(text, features="html.parser")
+class LocalPageParsing:
+    def __init__(self):
+        self.__summary = {}
+        self.__pars()
 
-book_name = soup.find("h1", class_="page-header__title").text.split(".")[0].strip()  # название книги
-author_name = soup.find("h1", class_="page-header__title").text.split(".")[1].strip()  # имя автора
-shop_price = soup.find("span", class_="rubs").text.replace(" ","")  # цена в магазине
-internet_price = soup.find("span", class_="silver rubs rubfont").text.replace(" ","")  # цена на сайте
-year = soup.find_all("dt", class_="book__details-value")[1].text.split()[0]  # год издания
-publisher = soup.find_all("dl", class_="book__details-item")[0].text.split()[1]  # издатель
-link = soup.find("link").get("href")  # ссылка на книгу
-pages = soup.find_all("dl", class_="book__details-item")[11].text.split()[1]  # количество страниц в книге
-product_code = soup.find_all("dl", class_="book__details-item")[12].text.split()[2]  # код товара
-vendor_code = soup.find_all("dl", class_="book__details-item")[13].text.split()[1]  # артикул
-isbn = soup.find_all("dl", class_="book__details-item")[14].text.split()[1]  # ISBN
+    def __pars(self):
+        page = urllib.request.urlopen("file:///home/evgeniy/Documents/html/book_parsing.html")
+        text = page.read()
+        soup = BeautifulSoup(text, features="html.parser")
 
-summary["book_name"] = book_name
-summary["author_name"] = author_name
-summary["shop_price"] = shop_price
-summary["internet_price"] = internet_price
-summary["the_year_of_publishing"] = year
-summary["publisher"] = publisher
-summary["link"] = link
-summary["pages"] = pages
-summary["product_code"] = product_code
-summary["vendor_code"] = vendor_code
-summary["isbn"] = isbn
+        book_name = soup.find("h1", class_="page-header__title").text.split(".")[0].strip()  # название книги
+        author_name = soup.find("h1", class_="page-header__title").text.split(".")[1].strip()  # имя автора
+        shop_price = soup.find("span", class_="rubs").text.replace(" ","")  # цена в магазине
+        internet_price = soup.find("span", class_="silver rubs rubfont").text.replace(" ","")  # цена на сайте
+        year = soup.find_all("dt", class_="book__details-value")[1].text.split()[0]  # год издания
+        publisher = soup.find_all("dl", class_="book__details-item")[0].text.split()[1]  # издатель
+        link = soup.find("link").get("href")  # ссылка на книгу
+        pages = soup.find_all("dl", class_="book__details-item")[11].text.split()[1]  # количество страниц в книге
+        product_code = soup.find_all("dl", class_="book__details-item")[12].text.split()[2]  # код товара
+        vendor_code = soup.find_all("dl", class_="book__details-item")[13].text.split()[1]  # артикул
+        isbn = soup.find_all("dl", class_="book__details-item")[14].text.split()[1]  # ISBN
+
+        self.__summary["book_name"] = book_name
+        self.__summary["author_name"] = author_name
+        self.__summary["shop_price"] = shop_price
+        self.__summary["internet_price"] = internet_price
+        self.__summary["the_year_of_publishing"] = year
+        self.__summary["publisher"] = publisher
+        self.__summary["link"] = link
+        self.__summary["pages"] = pages
+        self.__summary["product_code"] = product_code
+        self.__summary["vendor_code"] = vendor_code
+        self.__summary["isbn"] = isbn
+
+    def pars_dictionary(self):
+        return self.__summary
 
 
-print(summary)
-
+a1 = LocalPageParsing()
+a1.pars_dictionary()
--- users/EANabatov/TR_parsing/main.py
+++ users/EANabatov/TR_parsing/main.py
@@ -1,28 +1,122 @@
+import time
+
 from bs4 import BeautifulSoup
 import urllib.request
 
-book_name = []
-shop_price = []
-internet_price = []
-publisher = []
-info = []
 
-for index in range(1156290, 1156300):
-    try:
-        page = urllib.request.urlopen("https://www.moscowbooks.ru/book/" + str(index) + "/")
-        if page.getcode() == 200:
-            text = page.read()
-            soup = BeautifulSoup(text, features="html.parser")
-            book_name.append(soup.find("h1"))  # название книги
-            shop_price.append(soup.find("span", class_="rubs"))  # цена в магазине
-            shop_price.append(soup.find("span", class_="silver rubs rubfont"))  # цена на сайте
-            publisher.append(soup.find_all("dt", class_="book__details-value"))  # год и место издания
-            info.append(soup.find_all("a"))  # издательство
-    except:
-        pass
-# print(publisher)
-print(info[0])
+class BookStoreParser:
+    def __init__(self):
+        self.__books = []
+        self.__parsing()
 
-# soup = BeautifulSoup(text, features="html.parser")
-# list_tag_h1 = soup.findAll("h1")
-# print(list_tag_h1[0].text)
+    def __parsing(self):
+        __cw = 0
+        for index in range(1156290, 1156291):
+            __summary = {}
+            # time.sleep(1)
+            try:
+                page = urllib.request.urlopen("https://www.moscowbooks.ru/book/" + str(index) + "/")
+                text = page.read()
+                soup = BeautifulSoup(text, features="html.parser")
+                if page.getcode() == 200:
+                    try:
+                        __summary["book_name"] = self.__book_name()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["author_name"] = self.__author_name()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["shop_price"] = self.__shop_price()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["internet_price"] = self.__internet_price()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["the_year_of_publishing"] = self.__year_of_publishing()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["publisher"] = self.__publisher()
+                    except:
+                        print("book_name not gound")
+                    try:
+                    __summary["link"] = self.__link()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["pages"] = self.__pages()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["product_code"] = self.__product_code()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["vendor_code"] = self.__vendor_code()
+                    except:
+                        print("book_name not gound")
+                    try:
+                        __summary["isbn"] = self.__isbn()
+                    except:
+                        print("book_name not gound")
+                else:
+                    print(page.getcode())
+            except:
+                print(index, "skiped.", "Download: ", __cw)
+            if __summary != {}:
+                self.__books.append(__summary)
+                __cw = __cw + 1
+                print(__cw)
+
+    def return_books(self):
+        print(self.__books)
+
+    def __book_name(self):
+        return self.soup.find("h1", class_="page-header__title").text.split(".")[0].strip()  # название книги
+
+    def __author_name(self):
+        return self.soup.find("h1", class_="page-header__title").text.split(".")[1].strip()  # имя автора
+
+    def __shop_price(self):
+        return self.soup.find("span", class_="rubs").text.replace(" ","")  # цена в магазине
+
+    def __internet_price(self):
+        return self.soup.find("span", class_="silver rubs rubfont").text.replace(" ","")  # цена на сайте
+
+    def __year_of_publishing(self):
+        return self.soup.find_all("dt", class_="book__details-value")[1].text.split()[0]  # год издания
+
+    def __publisher(self):
+        return self.soup.find_all("dl", class_="book__details-item")[0].text.split()[1]  # издатель
+
+    def __link(self):
+        return self.soup.find("link").get("href")  # ссылка на книгу
+
+    def __pages(self):
+        return self.soup.find_all("dl", class_="book__details-item")[11].text.split()[1]  # количество страниц в книге
+
+    def __product_code(self):
+        return self.soup.find_all("dl", class_="book__details-item")[12].text.split()[2]  # код товара
+
+    def __vendor_code(self):
+        """Find vendor code"""
+        return self.soup.find_all("dl", class_="book__details-item")[13].text.split()[1]
+
+    def __isbn(self):
+        """Find ISBN"""
+        return self.soup.find_all("dl", class_="book__details-item")[14].text.split()[1]
+
+# # print(publisher)
+# print(info[0])
+#
+# # soup = BeautifulSoup(text, features="html.parser")
+# # list_tag_h1 = soup.findAll("h1")
+# # print(list_tag_h1[0].text)
+
+
+a2 = BookStoreParser()
+a2.return_books()
