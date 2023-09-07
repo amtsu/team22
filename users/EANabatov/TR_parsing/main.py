e7c5f890f7bc777f42405d8e0177e1394b3f09db
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-09-07 12:09:31 +0300

    в процессе

--- users/EANabatov/TR_parsing/main.py
+++ users/EANabatov/TR_parsing/main.py
@@ -23,46 +23,57 @@ class BookStoreParser:
                         summary["book_name"] = self.__book_name()
                     except:
                         print("book_name not found")
+                        summary["book_name"] = ""
                     try:
                         summary["author_name"] = self.__author_name()
                     except:
                         print("author_name not found")
+                        summary["author_name"] = ""
                     try:
                         summary["shop_price"] = self.__shop_price()
                     except:
                         print("shop_price not found")
+                        summary["shop_price"] = ""
                     try:
                         summary["internet_price"] = self.__internet_price()
                     except:
                         print("internet_price not found")
+                        summary["internet_price"] = ""
                     try:
                         summary["the_year_of_publishing"] = self.__year_of_publishing()
                     except:
                         print("the_year_of_publishing not found")
+                        summary["the_year_of_publishing"] = ""
                     try:
                         summary["publisher"] = self.__publisher()
                     except:
                         print("publisher not found")
+                        summary["publisher"] = ""
                     try:
                         summary["link"] = self.__link()
                     except:
                         print("link not found")
+                        summary["link"] = ""
                     try:
                         summary["pages"] = self.__pages()
                     except:
                         print("pages not found")
+                        summary["pages"] = ""
                     try:
                         summary["product_code"] = self.__product_code()
                     except:
                         print("product_code not found")
+                        summary["product_code"] = ""
                     try:
                         summary["vendor_code"] = self.__vendor_code()
                     except:
                         print("vendor_code not found")
+                        summary["vendor_code"] = ""
                     try:
                         summary["isbn"] = self.__isbn()
                     except:
                         print("isbn not found")
+                        summary["isbn"] = ""
                 else:
                     print(self.page.getcode())
             except:
