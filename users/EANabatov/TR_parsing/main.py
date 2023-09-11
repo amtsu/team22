c4467e1d9b071ed11a997922804343ba132e2bb4
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-09-11 18:07:42 +0300

    почти готовое дз по парсингу онлайн страницы с запросами на сервер

--- users/EANabatov/TR_parsing/main.py
+++ users/EANabatov/TR_parsing/main.py
@@ -1,127 +1,184 @@
-import time
+"""модуль парсинга сайта книжного магазина (онлайн)"""
 
 from bs4 import BeautifulSoup
 import urllib.request
 
 
 class BookStoreParser:
-    def __init__(self):
-        self.__books = []
-        self.__parsing()
+    """класс парсинга сайта книжного магазина"""
 
-    def __parsing(self):
-        cw = 0
-        for index in range(1156290, 1156300):
-            summary = {}
+    def __init__(self, start_parsing_index, end_parsing_index):
+        self.__books = []
+        self.__parsing(start_parsing_index, end_parsing_index)
+
+    def __parsing(self, start_parsing_index, end_parsing_index):
+        for index in range(start_parsing_index, end_parsing_index + 1):
+            __summary = {}
             try:
-                self.page = urllib.request.urlopen("https://www.moscowbooks.ru/book/" + str(index) + "/")
-                time.sleep(1)
+                self.page = urllib.request.urlopen(
+                    "https://www.moscowbooks.ru/book/" + str(index) + "/"
+                )
+                # time.sleep(1)
                 self.text = self.page.read()
                 self.soup = BeautifulSoup(self.text, features="html.parser")
                 if self.page.getcode() == 200:
                     try:
-                        summary["book_name"] = self.__book_name()
+                        __summary["in_stock"] = self.__in_stock()
                     except:
-                        print("book_name not found")
-                        summary["book_name"] = ""
+                        __summary["in_stock"] = ""
                     try:
-                        summary["author_name"] = self.__author_name()
+                        __summary["book_name"] = self.__book_name()
                     except:
-                        print("author_name not found")
-                        summary["author_name"] = ""
+                        __summary["book_name"] = ""
                     try:
-                        summary["shop_price"] = self.__shop_price()
+                        __summary["author_name"] = self.__author_name()
                     except:
-                        print("shop_price not found")
-                        summary["shop_price"] = ""
+                        __summary["author_name"] = ""
                     try:
-                        summary["internet_price"] = self.__internet_price()
+                        __summary["shop_price"] = self.__shop_price()
                     except:
-                        print("internet_price not found")
-                        summary["internet_price"] = ""
+                        __summary["shop_price"] = ""
                     try:
-                        summary["the_year_of_publishing"] = self.__year_of_publishing()
+                        __summary["internet_price"] = self.__internet_price()
                     except:
-                        print("the_year_of_publishing not found")
-                        summary["the_year_of_publishing"] = ""
+                        __summary["internet_price"] = ""
                     try:
-                        summary["publisher"] = self.__publisher()
+                        __summary[
+                            "the_year_of_publishing"
+                        ] = self.__year_of_publishing()
                     except:
-                        print("publisher not found")
-                        summary["publisher"] = ""
+                        __summary["the_year_of_publishing"] = ""
                     try:
-                        summary["link"] = self.__link()
+                        __summary["publisher"] = self.__publisher()
                     except:
-                        print("link not found")
-                        summary["link"] = ""
+                        __summary["publisher"] = ""
                     try:
-                        summary["pages"] = self.__pages()
+                        __summary["link"] = self.__link()
                     except:
-                        print("pages not found")
-                        summary["pages"] = ""
+                        __summary["link"] = ""
                     try:
-                        summary["product_code"] = self.__product_code()
+                        __summary["product_code"] = self.__product_code()
                     except:
-                        print("product_code not found")
-                        summary["product_code"] = ""
+                        __summary["product_code"] = ""
                     try:
-                        summary["vendor_code"] = self.__vendor_code()
+                        __summary["vendor_code"] = self.__vendor_code()
                     except:
-                        print("vendor_code not found")
-                        summary["vendor_code"] = ""
+                        __summary["vendor_code"] = ""
                     try:
-                        summary["isbn"] = self.__isbn()
+                        __summary["isbn"] = self.__isbn()
                     except:
-                        print("isbn not found")
-                        summary["isbn"] = ""
+                        __summary["isbn"] = ""
+                    try:
+                        __summary["pegi"] = self.__pegi()
+                    except:
+                        __summary["pegi"] = ""
                 else:
                     print(self.page.getcode())
             except:
-                print(index, "skiped.", "Download: ", self.cw)
+                pass
 
-            if summary != {}:
-                self.__books.append(summary)
-                cw = cw + 1
-                print(cw)
+            if __summary != {}:
+                self.__books.append(__summary)
 
     def return_books(self):
         print(self.__books)
 
     def __book_name(self):
-        return self.soup.find("h1", class_="page-header__title").text.split(".")[0].strip()  # название книги
+        """название книги"""
+        return (
+            self.soup.find("h1", class_="page-header__title")
+            .text.replace(" ", " ")
+            .strip()
+        )
 
     def __author_name(self):
-        return self.soup.find("h1", class_="page-header__title").text.split(".")[1].strip()  # имя автора
+        """имя автора (отсутствует если это журнал)"""
+        return self.soup.find("a", class_="author-name").text
 
     def __shop_price(self):
-        return self.soup.find("span", class_="rubs").text.replace(" ","")  # цена в магазине
+        """цена в магазине (отсутствует если товара нет в наличии)"""
+        return self.soup.find("span", class_="rubs").text.replace(" ", "").strip()
 
     def __internet_price(self):
-        return self.soup.find("span", class_="silver rubs rubfont").text.replace(" ","")  # цена на сайте
+        """цена на сайте (отсутствует если товара нет в наличии)"""
+        return (
+            self.soup.find("span", class_="silver rubs rubfont")
+            .text.replace(" ", "")
+            .strip()
+        )
 
     def __year_of_publishing(self):
-        return self.soup.find_all("dt", class_="book__details-value")[1].text.split()[0]  # год издания
+        """год издания книги (нет если книга не в наличии)"""
+        __result = ""
+        book_details = self.soup.find_all("dt", class_="book__details-value")
+        for detail in book_details:
+            if len(detail.text.replace(" ", " ").strip()) == 4:
+                __result = detail.text.replace(" ", " ").strip()
+                break
+        return __result
 
     def __publisher(self):
-        return self.soup.find_all("dl", class_="book__details-item")[0].text.split()[1]  # издатель
+        """находит издателя"""
+        return self.soup.find_all("dt", class_="book__details-value")[0].text.strip()
 
     def __link(self):
+        """находит ссылку на страницу с книгой"""
         return self.soup.find("link").get("href")  # ссылка на книгу
 
-    def __pages(self):
-        return self.soup.find_all("dl", class_="book__details-item")[11].text.split()[1]  # количество страниц в книге
-
     def __product_code(self):
-        return self.soup.find_all("dl", class_="book__details-item")[12].text.split()[2]  # код товара
+        """находит код продукта"""
+        __result = ""
+        book_details = self.soup.find_all("dt", class_="book__details-value")
+        for detail in book_details:
+            if len(detail.text.strip()) == 7:
+                __result = detail.text.strip()
+        return __result
 
     def __vendor_code(self):
-        """Find vendor code"""
-        return self.soup.find_all("dl", class_="book__details-item")[13].text.split()[1]
+        """находит артикул"""
+        __result = ""
+        book_details = self.soup.find_all("dt", class_="book__details-value")
+        for detail in book_details:
+            if detail.text.strip()[0] == "К" and len(detail.text.strip()) == 6:
+                __result = detail.text.strip()
+        return __result
 
     def __isbn(self):
-        """Find ISBN"""
-        return self.soup.find_all("dl", class_="book__details-item")[14].text.split()[1]
+        """находит международный идентификатор книги (ISBN)"""
+        __result = ""
+        book_details = self.soup.find_all("dt", class_="book__details-value")
+        for detail in book_details:
+            if len(detail.text.replace("-", "").strip()) == 13:
+                __result = detail.text.replace("-", "").strip()
+        return __result
+
+    def __in_stock(self):
+        """определяет в наличии ли книга в магазине на данный момент"""
+        __stock = None
+        try:
+            __stock = (
+                self.soup.find("span", class_="book__shop-instock")
+                .text.replace(" ", " ")
+                .strip()
+            )
+        except:
+            __stock = (
+                self.soup.find("span", class_="instock1")
+                .text.replace(" ", " ")
+                .strip()
+                .lower()
+            )
+        return __stock
+
+    def __pegi(self):
+        """находит возрастное ограничение для книг (PEGI)"""
+        __result = ""
+        book_details = self.soup.find_all("dt", class_="book__details-value")
+        for detail in book_details:
+            if detail.text.strip()[::-1][0] == "+":
+                __result = detail.text.strip()
+        return __result
 
 
-a2 = BookStoreParser()
+a2 = BookStoreParser(1156290, 1156297)
 a2.return_books()
