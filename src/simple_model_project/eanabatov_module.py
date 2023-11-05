b96a658f0e14c863202c974fffbc98cb127bc851
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-11-05 22:37:10 +0300

    доработанный модуль под требования из пула: https://github.com/amtsu/team22/pull/182

--- src/simple_model_project/eanabatov_module.py
+++ src/simple_model_project/eanabatov_module.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 
 import logging
 from random import random, randint
-from typing import Tuple
+from typing import Dict
 
 from abstractmodule import AbstractModule
 
 
 class EANabatovModule(AbstractModule):
-    """реализация абстрактного класса с использованием паттерна Singleton"""
+    """Реализация абстрактного класса с использованием паттерна Singleton"""
 
     def __new__(cls):
         """Реализация Singleton"""
@@ -19,42 +19,56 @@ class EANabatovModule(AbstractModule):
 
     def __init__(self):
         """Инициализация атрибутов класса"""
-        self.__status = True
         logging.basicConfig(
-            level=logging.DEBUG,
+            level=logging.INFO,
             filename="eanabatov_py_log.log",
             filemode="w",
             format="%(asctime)s %(levelname)s %(message)s",
         )
+        self.__status: bool = True
+        self.__counter: int = 0
+        self.__limit: int = 5
+        self.__state: dict[str, str] = {}
 
     def name(self) -> str:
-        """возвращает название класса"""
+        """Возвращает название класса"""
         logging.info("name is done")
         return self.__class__.__name__
 
-    def prepare(self):
+    def prepare(self) -> None:
         """Выполняется перед началом цикла вызова методов step"""
         self.__status = False
         logging.info("prepare is done")
 
-    def step(self) -> Tuple[float, int]:
+    def step(self) -> None:
         """Возвращает состояние модуля на данном шаге,
         выполняется на каждом шаге, пока разрешено"""
-        __step = None
-        if self.__status is False:
-            __step = (randint(1, 10) + random(), randint(1, 100))
+        self.__counter += 1
+        if self.__counter <= self.__limit and self.__status is False:
+            self.__state = {
+                "var A": str(randint(1, 10) + random()),
+                "var B": str(randint(1, 100)),
+            }
+            logging.info("step is done. " "counter is: %s", self.__counter)
+            if self.__counter >= self.__limit:
+                self.__status = True
         else:
-            logging.critical("self.__status is True")
-        logging.info("step is done")
-        return __step
+            self.__counter -= 1
+            self.__status = True
+            logging.critical("self.__status is %s", self.__status)
+            logging.info("step dont done. " "counter is: %s", self.__counter)
+        logging.info("step is over")
+
+    def get_state(self) -> Dict[str, str]:
+        logging.info("get_state is done (counter is %s)", self.__counter)
+        return self.__state
 
     def is_done(self) -> bool:
-        """Сигнализирует о окончании процесса моделирования"""
-        self.__status = True
+        """Сигнализирует об окончании процесса моделирования"""
         logging.info("is_done is done :)")
         return self.__status
 
 
 def create_instance() -> EANabatovModule:
-    """создает экземпляр класса и возвращает его в виде объекта"""
+    """Создает экземпляр класса и возвращает его в виде объекта"""
     return EANabatovModule()
