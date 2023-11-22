216531d911c272e2b32f9594bda16db384f2d327
Evgeny Nabatov <122977103+EANabatov@users.noreply.github.com>
2023-11-22 22:44:53 +0300

    EANabatov_Model_v2 (#184)
    * доработанный модуль
    под требования из пула: https://github.com/amtsu/team22/pull/182
    
    * доработанный модуль
    под требования из пула: https://github.com/amtsu/team22/pull/182
    mypy + pylint + black

--- src/simple_model_project/eanabatov_test.py
+++ src/simple_model_project/eanabatov_test.py
@@ -23,10 +23,48 @@ class Test:
         assert create_instance().name() == "EANabatovModule"
 
     def test_main_work(self):
-        """проверка основного функционала класса"""
+        """Проверка основного функционала класса"""
         test_object = EANabatovModule()
         test_object.prepare()
-        test_step = test_object.step()
-        assert isinstance(test_step[0], float)
-        assert isinstance(test_step[1], int)
+        test_object.step()
+        assert test_object.is_done() is False
+        test_object.step()
+        assert test_object.is_done() is False
+        test_object.step()
+        assert test_object.is_done() is False
+        test_object.step()
+        assert test_object.is_done() is False
+        test_object.step()
         assert test_object.is_done() is True
+
+    def test_get_state(self, monkeypatch):
+        """Тест проверки процедуры get_state()"""
+
+        def monk_state():
+            return {"var A": "10.235667", "var B": "73"}
+
+        monkeypatch.setattr(target=EANabatovModule, name="get_state", value=monk_state)
+
+        test_object = EANabatovModule()
+        test_object.prepare()
+        test_object.step()
+        assert test_object.get_state() == {"var A": "10.235667", "var B": "73"}
+
+    def test_step_and_prepare(self):
+        """Тест проверки процедуры step() и prepare()"""
+        test_object = EANabatovModule()
+        test_object.step()
+        assert test_object.is_done() is True
+        assert test_object.get_state() == {}
+
+    def test_is_done(self):
+        """Тест проверки процедуры step()"""
+        test_object = EANabatovModule()
+        test_object.prepare()
+        test_counter: int = 0
+        assert test_object.is_done() is False
+        while test_counter <= 5:
+            test_counter += 1
+            test_object.step()
+        assert test_object.is_done() is True
+        assert test_object.get_state() != {}
