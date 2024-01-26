1188346b5c1390f77816e4ea031def7e9c0e4d10
Andromary <113727171+Andromary@users.noreply.github.com>
2024-01-26 13:22:24 +0300

    mvandreeva added hals_parser (#154)
    * mvandreeva added hals_parser
    
    * fixed some errors
    
    * Aded dags and made some changes
    
    * Some chenges due to black, mypy, pylint
    
    * Add offline tests
    
    * add tags to dags
    
    * Added _get_location
    
    * replaced misspell, updated decorator
    
    * removed decorator to another class method
    
    * extended tests
    
    * fixed dag

--- users/mvandreeva/project_equilibrium/developers/hals/page_parsing.py
+++ users/mvandreeva/project_equilibrium/developers/hals/page_parsing.py
@@ -1,14 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib
+import urllib.request
+
 """
 Парсер для сайта застройщика 'CapitalGroup', проект 'Триколор'
 """
-import urllib
-import urllib.request
-from bs4 import BeautifulSoup
 
-class PageParser:
+
+class PagePerser:
     """
     Производит открытие страницы, её первичную обработку
     """
+
     def __init__(self, url: str):
         self.__url = url
         self.__page = ""
@@ -20,13 +23,10 @@ class PageParser:
         try:
             with urllib.request.urlopen(self.__url) as page:
                 self.__page = page.read()
-        except (urllib.request.HTTPError, urllib.request.URLError):
+        except:
             print("Error url =", self.__url)
         return self.__page
 
     def use_b_soup(self):
-        """
-        Создает объект класса BeautifulSoup
-        """
         b_soup = BeautifulSoup(self.open_page(), features="html.parser")
         return b_soup
