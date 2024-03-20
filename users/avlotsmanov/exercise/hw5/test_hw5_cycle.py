bf39727b8fec44df8078a8456bb49225afdb8284
avlotsmanov <misterdx1990@gmail.com>
2024-03-20 16:23:20 +0300

    avlotsmanov - HW9 - раскидал функции из hw_5  по файлам и накидал места для тестов

---
+++ users/avlotsmanov/exercise/hw5/test_hw5_cycle.py
@@ -0,0 +1,14 @@
+import pytest
+from hw5_cycle import *
+
+#тесты функции print_squares_while
+
+#тесты функции print_factorial
+
+#тесты функции sum_in_range
+
+#тесты функции primes
+
+#тесты функции is_it_pallindrom
+
+#тесты функции gsv
