121598eab3e646967182a32721c3c2014990d13f
arisova <anna.arisova@gmail.com>
2024-04-02 16:07:16 +0300

    Позитивные тесты к hw-5-part_1-4

--- users/aaarisova/exercise/hw-5/test-5/functions_hw_5.py
+++ users/aaarisova/exercise/hw-5/test-5/functions_hw_5.py
@@ -1,60 +1,49 @@
-'''I. ИЗ hw-5-part-1.ipynb '''
+# I. Множества из hw-5-part-1 
 
-def create_set_1():
-    '''Создает множество set1 из целых чисел: 1, 2, 3, 4, 5'''
-    set1 = {1, 2, 3, 4, 5}
+def create_set_1(numbers):
+    '''функция создает множество set1 из целых чисел'''
+    set1 = set(numbers)
     return set1
 
-set1 = {1, 2, 3, 4, 5}
-def add_element_to_set(set1):
-    '''Добавляем в это множество элемент 6'''
-    set1.add(6)
+
+def add_element_to_set(set1, elem):
+    '''функция добавляет в множество элемент '''
+    set1.add(elem)
     return set1
 
-def remove_element_from_set(set1):
-    '''Удаляем элемент 2 из множества '''
-    set1.remove(2)
+def remove_element_from_set(set1, elem):
+    '''функция удаляет элемент из множества '''
+    set1.remove(elem)
     return set1  
 
 
-x = 3
-def has_element_in_set(set1, x):
-    '''Проверьте, содержит ли множество элемент 3'''
-    result = x in set1
+def has_element_in_set(set1, elem):
+    '''функция проверяет, содержит ли множество элемент'''
+    result = elem in set1
     return result
 
 
-def create_set_2():
-    '''Создайте второе множество set2 с элементами 4, 5, 6, 7, 8'''
-    set2 = {4, 5, 6, 7, 8}
+def create_set_2(elements):
+    '''функция создает второе множество set2 с элементами'''
+    set2 = set(elements)
     return set2
 
-set2 = {4, 5, 6, 7, 8}
+
 def find_union_sets(set1, set2):
-    '''Найдите объединение множеств set1 и set2'''
+    '''функция находит объединение множеств set1 и set2'''
     union_sets = set1 | set2
     return union_sets 
 
 
 def difference_sets(set1, set2):
-    '''Найдите разность множеств set1 и set2'''
+    '''функция нахождения разности множеств set1 и set2'''
     difference = set1 - set2
     return difference
 
-
 def create_sets_of_rainbows(set1, set2):
-    ''' Создайте два множества set1 и set2, содержащие цвета радуги: 
-    "красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый" 
-    и "оранжевый", "зеленый", "голубой", "синий", "Мальвиновый".'''
-
-    set1 = {"красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый"}
-    set2 = {"оранжевый", "зеленый", "голубой", "синий", "Мальвиновый"}
+    '''функция создает два множества'''
     return set1, set2
-
-
-set1 = {"красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый"}
-set2 = {"оранжевый", "зеленый", "голубой", "синий", "Мальвиновый"}
-
+    
 def intersection_of_sets(set1, set2):
     '''Найдите пересечение множеств set1 и set2'''
     intersection_set = set1 & set2
@@ -68,6 +57,69 @@ def union_sets_in_new_set(set1, set2):
 
 
 
-'''II. ИЗ hw-5-part-2.ipynb '''
+#II. Словари из hw-5-part-2
 
+def create_dict_person(dict_person):
+    '''функция создания словарь'''
+    return dict_person
+
+def add_email_to_dict(person_dict, email):
+    '''функция добавления в словарь нового ключа со значением электронного адреса'''
+    person_dict["email"] = email
+    return person_dict 
+
+
+def del_key_to_dict(person_dict, del_key):
+    '''функция удаления ключа из словаря.'''
+    del person_dict[del_key]
+    return person_dict
+
+
+def dict_total_of_fruits(fruits_quantity, fruits_prices):
+    '''функция подсчета стоимости всех фруктов, имеющихся в наличии'''
+    total_cost = sum(fruits_quantity[fruit] * fruits_prices[fruit] for fruit in fruits_quantity)
+    return total_cost
+
+
+def add_stud_to_dict(dict_stud, surname, age):
+    '''функция добавления нового студента c возрастом в словарь.'''
+    dict_stud[surname] = age
+    return dict_stud
+
+
+#III, IV. Условные операторы, циклы
+
+def adult_person(person):
+    '''функция выводит сообщение "Совершеннолетний" или "Несовершеннолетний".'''
+    age = int(person['age'])
+    if age >= 18:
+        return 'Совершеннолетний'
+    else:
+        return 'Несовершеннолетний'
+
+
+def compare_sets(set1, set2):
+    '''функция сравнивает длину двух множеств и выводит сообщение о том, какое из множеств больше.'''
+
+    x = len(set1) 
+    y = len(set2)
+    
+    if x > y:
+        print("set1 > set2") 
+        return "set1 > set2"
+    elif x < y:
+        print("set1 < set2")
+        return "set1 < set2"
+    else:
+        print("set1 = set2")
+        return "set1 = set2"
+
+def price_is_over_limit(fruits_prices, num):
+    '''Выведите сообщение "Дорогой товар" для продуктов, цена которых превышает указанное число'''
+    expensive_fruits = []
+    for fruit, price in fruits_prices.items(): 
+        if price > num:
+            expensive_fruits.append(fruit)
+            print(f"Дорогой товар: {fruit}")
+    return expensive_fruits
 
--- users/aaarisova/exercise/hw-5/test-5/test_hw_5.py
+++ users/aaarisova/exercise/hw-5/test-5/test_hw_5.py
@@ -1,68 +1,65 @@
 import pytest
-from functions_hw_5 import *
+from functions_hw_5 import (create_set_1, add_element_to_set, remove_element_from_set, has_element_in_set, create_set_2, find_union_sets, difference_sets, create_sets_of_rainbows, intersection_of_sets, union_sets_in_new_set, create_dict_person, add_email_to_dict, del_key_to_dict, dict_total_of_fruits, add_stud_to_dict, adult_person, compare_sets, price_is_over_limit) 
 
+
+# I.
 
 def test_create_set_1():
-    '''тест создания множества set1 из целых чисел: 1, 2, 3, 4, 5'''
-    set1 = create_set_1()
-    assert set1 == {1, 2, 3, 4, 5}, 'Ошибка создания множества'
+    '''тест функции - создание множество set1 из целых чисел'''
+    numbers = [1, 2, 3, 4, 5]
+    assert create_set_1(numbers) == {1, 2, 3, 4, 5}
 
 
 def test_add_element_to_set():
     '''Тест-проверка добавен ли в это множество элемент 6'''
     set1 = {1, 2, 3, 4, 5}
-    set1 = add_element_to_set(set1)
-    assert set1 == {1, 2, 3, 4, 5, 6}, 'Ошибка добавления элемента'
+    elem = 6
+    assert add_element_to_set(set1, elem) == {1, 2, 3, 4, 5, 6}, 'Ошибка добавления элемента'
 
 
 def test_remove_element_from_set():
     '''Тест удален ли элемент 2 из множества '''
     set1 = {1, 2, 3, 4, 5, 6}
-    set1 = remove_element_from_set(set1)
-    assert set1 == {1, 3, 4, 5, 6}, 'Ошибка удаления элемента'
+    elem = 2
+    assert remove_element_from_set(set1, elem) == {1, 3, 4, 5, 6}, 'Ошибка удаления элемента'
 
 
 def test_has_element_in_set():
-    '''Проверьте, содержит ли множество элемент 3'''
+    '''тест. проверяем, содержит ли множество элемент 3'''
     set1 = {1, 3, 4, 5, 6}
-    result = has_element_in_set(set1, x)
-    assert result == True, 'Ошибка в содержании элемента 3 в множестве'
+    assert has_element_in_set(set1, 3) == True, 'Ошибка в содержании элемента 3 в множестве'
 
 
 def test_create_set_2():
     '''Тест на создание второго множества set2 с элементами 4, 5, 6, 7, 8'''
-    set2 = create_set_2()
-    assert set2 == {4, 5, 6, 7, 8}, 'Ошибка создания множества'
+    elements = [4, 5, 6, 7, 8]
+    assert create_set_2(elements) == {4, 5, 6, 7, 8}, 'Ошибка создания множества'
 
  
 def test_find_union_sets():
     '''Тест на объединение множеств set1 и set2'''
     set1 = {1, 3, 4, 5, 6}
     set2 = {4, 5, 6, 7, 8}
-    result = find_union_sets(set1, set2)
-    assert result == {1, 3, 4, 5, 6, 7, 8}  
-
+    assert find_union_sets(set1, set2) == {1, 3, 4, 5, 6, 7, 8}, 'Ошибка объединения множеств'   
 
 
 def test_difference_sets():
-    '''Найдите разность множеств set1 и set2'''
+    '''тест нахождения разности множеств set1 и set2'''
     set1 = {1, 3, 4, 5, 6}
     set2 = {4, 5, 6, 7, 8}
-    result = difference_sets(set1,set2)
-    assert result == {1, 3}
+    assert difference_sets(set1,set2) == {1, 3}, 'ошибка нахождения разности множеств'
+
 
 
 def test_create_sets_of_rainbows():
-    ''' Создайте два множества set1 и set2, содержащие цвета радуги: 
-    "красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый" 
-    и "оранжевый", "зеленый", "голубой", "синий", "Мальвиновый".'''
+    ''' Тест на создание два множества, содержащие цвета радуги.'''
 
     set1 = {"красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый"}
     set2 = {"оранжевый", "зеленый", "голубой", "синий", "Мальвиновый"}
     result = create_sets_of_rainbows(set1, set2)
+    
     assert result == ({'голубой','желтый','зеленый','красный','оранжевый','синий','фиолетовый'},
  {'Мальвиновый', 'голубой', 'зеленый', 'оранжевый', 'синий'}) 
-
 
  
 def test_intersection_of_sets():
@@ -84,4 +81,67 @@ def test_union_sets_in_new_set():
 
     result = union_sets_in_new_set(set1, set2)
     assert result == {'Мальвиновый','голубой','желтый','зеленый','красный','оранжевый','синий','фиолетовый'}
+
+
+# II.
+
+def test_create_dict_person():
+    '''тест создания словарь'''
+    dict_person = {"name": "Ivan", "age": "26", "city": "Moscow"}
+    assert create_dict_person(dict_person) == {"name": "Ivan", "age": "26", "city": "Moscow"}, 'Ошибка создания словаря'
+
+
+def test_add_email_to_dict():
+    '''тест на добавление в словарь нового ключа со значением электронного адреса'''
+    person_dict = {'name': 'Ivan', 'age': '26', 'city': 'Moscow'}
+    email = 'anna.arisova@gmail.com'
+    assert add_email_to_dict(person_dict, email) == {'name': 'Ivan', 'age': '26', 'city': 'Moscow', 'email': 'anna.arisova@gmail.com'}, 'Ошибка добавления ключа "email" в словарь.'
+
+
+def test_del_key_to_dict():
+    '''тест функции для удаления ключа из словаря.'''
+    person_dict = {'name': 'Ivan', 'age': '26', 'city': 'Moscow', 'email': 'anna.arisova@gmail.com'}
+    del_key = 'city'
+    assert del_key_to_dict(person_dict, del_key) == {'name': 'Ivan', 'age': '26', 'email': 'anna.arisova@gmail.com'}, 'Ошибка удаления ключа "city" из словаря.'
+
+
+def test_dict_total_of_fruits():
+    '''тест подсчета стоимости всех фруктов, имеющихся в наличии'''
+    fruits_quantity = fruits_quantity = {"яблоко": 5, "банан": 10, "апельсин": 7}
+    fruits_prices = {"яблоко": 2, "банан": 1, "апельсин": 3}
+    assert dict_total_of_fruits(fruits_quantity, fruits_prices) == 41, 'Ошибка подсчета стоимости всех фруктов, имеющихся в наличии'
+
+
+def test_add_stud_to_dict():
+    '''тест функции добавления нового студента "Смирнова" возрастом 20 лет в словарь.'''
+    dict_stud = {"Иванов": 22, "Петрова": 13, "Сидоров": 23}
+    surname = 'Смирнов'
+    age = 20
+    assert add_stud_to_dict(dict_stud, surname, age) == {'Иванов': 22, 'Петрова': 13, 'Сидоров': 23, 'Смирнов': 20}, 'Ошибка добавления нового студента "Смирнова" в словарь.'
+
+
+
+#III, IV.
+
+def test_adult_person():
+    '''тест функции вывода сообщения "Совершеннолетний" или "Несовершеннолетний".'''
+    person = {"name": "Ivan", "age": "26", "city": "Moscow"}
+    assert adult_person(person) == 'Совершеннолетний'
+
+
+def test_compare_sets():
+    '''тест функции сравнения длины двух множеств и вывода сообщения о том, какое из множеств больше.'''
+
+    set1 = {"красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый"}
+    set2 = {"оранжевый", "зеленый", "голубой", "синий", "Мальвиновый"}
+    assert compare_sets(set1, set2) == 'set1 > set2' 
+    
+def test_price_is_over_limit():
+    ''' Тест для функции, которая выводит список продуктов, цена которых превышает указанное число'''
+    fruits_prices = {"яблоко": 1.5, "банан": 2, "апельсин": 1.2}
+    assert price_is_over_limit(fruits_prices, 1.4) == ["яблоко", "банан"], 'Ошибка в тесте'
+
+
+
+
 
