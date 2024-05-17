994dbe4433e91d4d0a6e9f05fe0f376c7b5f9c4e
rmarfenkova <hogo88@mail.ru>
2024-05-17 22:46:57 +0300

    часть 3 готова

--- users/rmarfenkova/exercise/hw13/test_13_part1_Car.py
+++ users/rmarfenkova/exercise/hw13/test_13_part1_Car.py
@@ -7,13 +7,13 @@ def test_load_from_file():
     # Сохраняем объект автомобиля в файл
     car.save_to_file("car_data.pkl")
     # Загружаем объект автомобиля из файла
-    loaded_car = Car.load_from_file("car_data.pkl")
+    loaded_car = car.load_from_file("car_data.pkl")
     # Проверяем, что объект успешно загружен
     assert isinstance(loaded_car, Car)
     # изменяем скорость автомобиля
     loaded_car.accelerate()
-    loaded_car.save_to_file("updated_car_data.pkl")
+    loaded_car.save_to_file("car_data.pkl")
     #Загружаем объект автомобиля из файла после изменения
-    updated_loaded_car = Car.load_from_file("updated_car_data.pkl")
+    updated_loaded_car = car.load_from_file("car_data.pkl")
     # Проверяем, что скорость была изменена
     assert updated_loaded_car.get_speed() == 5
