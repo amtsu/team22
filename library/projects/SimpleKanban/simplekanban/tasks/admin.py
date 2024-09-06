f57539ba3ae3a2a5ba6e36881425fcab50834552
arisova <147975010+arisova@users.noreply.github.com>
2024-09-06 10:41:50 +0200

    SimpleKanBan (#900)
    * Добавлен проект SimpleKanban
    
    * Создание проекта SimpleKanban
    
    * Обновление и перенос проекта: добавление и удаление файлов

---
+++ library/projects/SimpleKanban/simplekanban/tasks/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'get_execution_status_display', 'author', 'created_at', 'due_date')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
+    list_filter = ('execution_status', 'created_at')
+    ordering = ('created_at', 'due_date', 'execution_status', 'author')
+
+
+#admin.site.register(Task, TaskAdmin) - вместо этого декоратор @admin.register(Task)
