86ee4465f6875d478b8cb3f5c06a0259471b0ecf
Alexander Mareyev <adskiypovar@gmail.com>
2024-09-24 19:21:03 +0300

    bugs fixed, token mooved to env (#917)

--- library/projects/teleban/main.py
+++ library/projects/teleban/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import sys
+from os import getenv
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
@@ -9,60 +10,60 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from config import TOKEN
-from keyboard_main import start_ikb
-
-from db_managers.subscription_manager import SubscriptionDatabaseManager
-
-from src_sports_ru.handlers import sports_router
 from db_managers.content_manager import ContentDatabaseManager
-
+from db_managers.subscription_manager import SubscriptionDatabaseManager
+from keyboard_main import start_ikb
+from src_sports_ru.handlers import sports_router
 from src_sports_ru.text_data import NBA_TAGS_LIST
+
+TOKEN = getenv("TELEBAN_TOKEN")
 
 dp = Dispatcher(name=__name__)
 dp.include_routers(sports_router)
 
 
 async def send_out_content(bot: Bot):
-    new_content = None
-
+    """
+    Функция рассылки нового контента пользователям (пока только спортс.ру).
+    """
+    # проверка наличия нового контента
     with ContentDatabaseManager('content_sports_ru') as db:
-
         if db.check_new_content():
             new_content = db.get_new_content()
         else:
             return None
 
+    # перебор нового контента для рассылки
     for title, link, tags in new_content:
         tags = tags.split(',')
-        anti_duplicating_list = []
+        anti_duplicating_list = []  # храним историю рассылки, чтобы исключить дублирование
 
+        # перебор тегов новости
         for tag in tags:
-            if tag not in NBA_TAGS_LIST:
+            if tag not in NBA_TAGS_LIST:  # проверка, есть ли тег в предложенных для пользователя
                 continue
 
             with SubscriptionDatabaseManager() as db:
-
-                if not db.check_exist_subscription('sports', tag):
+                if not db.check_exist_subscription('sports', tag):  # проверка наличия подписчиков на тег
                     continue
                 else:
-                    user_list = db.get_tag_users('sports', tag)
+                    user_list = db.get_tag_users('sports', tag)  # получение пользователей для рассылки
 
+            # перебор пользователей и рассылка им новости
             for user_id in user_list:
-                if (user_id, link) in anti_duplicating_list:
+                if (user_id, link) in anti_duplicating_list:  # проверка, что новость ранее не высылалась
                     continue
 
+                # отправка сообщения
                 await bot.send_message(
                     chat_id=user_id,
                     text=f'<b>{title}</b>\n'
                          f'{link}'
                 )
-                anti_duplicating_list.append((user_id, link))
-
+                anti_duplicating_list.append((user_id, link))  # добавляем в историю рассылки
 
         with ContentDatabaseManager('content_sports_ru') as db:
-
-            db.update_status(link)
+            db.update_status(link)  # меняем статус на "Отправлено"
 
 
 @dp.message(CommandStart())
