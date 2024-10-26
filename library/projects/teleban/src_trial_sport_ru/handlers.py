3115f95b45e29ef3905da906ef59f3042d28e841
theRate <adskiypovar@gmail.com>
2024-10-27 01:17:46 +0300

    trial keyboards bug fix

--- library/projects/teleban/src_trial_sport_ru/handlers.py
+++ library/projects/teleban/src_trial_sport_ru/handlers.py
@@ -6,13 +6,14 @@ from src_trial_sport_ru.keyboards import trial_sport_ikb
 
 trial_sport_router = Router()
 
-@trial_sport_router.callback_query(F.data.startswith('trial-sport'))
-async def handle_subscription(callback: CallbackQuery, source: str, keyboard_func):
+
+@trial_sport_router.callback_query(F.data.startswith('trial-sport_'))
+async def handle_subscription(callback: CallbackQuery):
+    source = 'trial-sport'
     user_id = callback.from_user.id
     tag = callback.data.removeprefix(f'{source}_')
 
     with SubscriptionDatabaseManager() as db:
-
         user_tags = db.get_user_tags(user_id, source)
 
         if tag in user_tags:
@@ -24,8 +25,9 @@ async def handle_subscription(callback: CallbackQuery, source: str, keyboard_fun
             await callback.answer(f'Вы подписались на {tag})')
             await callback.message.edit_reply_markup(reply_markup=await trial_sport_ikb(user_id))
 
+
 @trial_sport_router.callback_query(F.data.startswith('trial-sport'))
-async def sports_callback(callback: CallbackQuery):
+async def trial_sports_callback(callback: CallbackQuery):
     user_id = callback.from_user.id
     data = callback.data
     match data:
